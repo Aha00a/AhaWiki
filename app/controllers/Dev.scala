82ddf35942708f6bbdef1d5e554a8af4e11adb60
aha00a <aha00a@gmail.com>
2015-10-17 22:29:27 +0900

    add redirect with flashing for dev urls.

--- app/controllers/Dev.scala
+++ app/controllers/Dev.scala
@@ -7,6 +7,7 @@ import actors.ActorPageProcessor.Calculate
 import akka.actor.ActorSystem
 import models.{DirectQuery, MockDb}
 import play.api.mvc._
+import utils.RequestUtil
 
 import scala.util.Random
 
@@ -15,21 +16,31 @@ class Dev @Inject()(system: ActorSystem) extends Controller {
   val actorSimilarPage = system.actorOf(ActorPageProcessor.props)
 
   def reset = Action { implicit request =>
+    val result = Redirect(RequestUtil.refererOrRoot(request))
     if(request.headers.get("Host").getOrElse("").startsWith("localhost")) {
       MockDb.pageFromFile().foreach(p => {
         DirectQuery.pageDelete(p.name)
         DirectQuery.pageInsert(p.name, p.revision, p.time, p.author, p.remoteAddress, p.content, p.comment.getOrElse(""))
         actorSimilarPage ! Calculate(p.name)
       })
+      result.flashing("success" -> "Reset Succeed.")
     }
-    Redirect("/").flashing("success" -> "Reset")
+    else
+    {
+      result.flashing("error" -> "Reset Failed")
+    }
   }
 
   def reindex = Action { implicit request =>
+    val result = Redirect(RequestUtil.refererOrRoot(request))
     if(request.headers.get("Host").getOrElse("").startsWith("localhost")) {
       Random.shuffle(DirectQuery.pageSelectNameGroupByNameOrderByName).foreach(s => actorSimilarPage ! Calculate(s))
+      result.flashing("success" -> "Reindex Succeed.")
     }
-    Ok("")
+    else
+    {
+      result.flashing("error" -> "Reindex Failed")
+    }
   }
 }
 
