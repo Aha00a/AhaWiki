b47147c9a3dfb8ec61379dba27f417a894ca5152
aha00a <aha00a@gmail.com>
2015-10-18 22:38:53 +0900

    introduce trait CacheEntity

--- app/logics/Cache.scala
+++ app/logics/Cache.scala
@@ -2,62 +2,43 @@ package logics
 
 import logics.wikis.Interpreters
 import models.{DirectQuery, MockDb, WikiContext}
-import play.api.Logger
 
 import scala.concurrent.duration._
 
 object Cache {
-
-  object PageList {
+  trait CacheEntity {
     val key: String = getClass.getName
+    def invalidate()(implicit wikiContext: WikiContext): Unit = {
+      wikiContext.cacheApi.remove(key)
+    }
+  }
 
+  object PageList extends CacheEntity {
     def get()(implicit wikiContext: WikiContext) = wikiContext.cacheApi.getOrElse(key, 60.minutes) {
-      Logger.info(s"Cache miss: $key")
       DirectQuery.pageSelectPageList()
     }
 
-    def invalidate()(implicit wikiContext: WikiContext): Unit = {
-      wikiContext.cacheApi.remove(key)
+    override def invalidate()(implicit wikiContext: WikiContext): Unit = {
+      super.invalidate()
       wikiContext.cacheApi.remove(PageNameSet.key)
     }
   }
 
-  object PageNameSet {
-    val key: String = getClass.getName
-
+  object PageNameSet extends CacheEntity {
     def get()(implicit wikiContext: WikiContext) = wikiContext.cacheApi.getOrElse(key, 60.minutes) {
       PageList.get().map(_.name).toSet
     }
-
-    def invalidate()(implicit wikiContext: WikiContext): Unit = {
-      wikiContext.cacheApi.remove(key)
-    }
   }
 
-  object Header {
-    val key: String = getClass.getName
-
+  object Header extends CacheEntity {
     def get()(implicit wikiContext: WikiContext) = wikiContext.cacheApi.getOrElse(key, 60.minutes) {
       Interpreters.interpret(MockDb.selectPageLastRevision(".header").map(_.content).getOrElse(""))
     }
-
-    def invalidate()(implicit wikiContext: WikiContext): Unit = {
-      wikiContext.cacheApi.remove(key)
-    }
-
   }
 
-  object Footer {
-    val key: String = getClass.getName
-
+  object Footer extends CacheEntity {
     def get()(implicit wikiContext: WikiContext) = wikiContext.cacheApi.getOrElse(key, 60.minutes) {
       Interpreters.interpret(MockDb.selectPageLastRevision(".footer").map(_.content).getOrElse(""))
     }
-
-    def invalidate()(implicit wikiContext: WikiContext): Unit = {
-      wikiContext.cacheApi.remove(key)
-    }
-
   }
-
 }
