eb1c31a52a407fa550de23827e75a8a1c392bce0
aha00a <aha00a@gmail.com>
2015-10-18 21:13:33 +0900

    split evolution file, change Page.content type to LONGTEXT

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -8,28 +8,11 @@ CREATE TABLE Page (
     time BIGINT,
     author TEXT,
     remoteAddress TEXT,
-    content TEXT,
+    content LONGTEXT,
     comment TEXT,
     PRIMARY KEY (name,revision)
 );
 
-
-CREATE TABLE TermFrequency (
-    name       VARCHAR(255),
-    term       VARCHAR(255),
-    frequency  INTEGER,
-    PRIMARY KEY (name, term)
-);
-
-CREATE TABLE CosineSimilarity (
-    name1 VARCHAR(255),
-    name2 VARCHAR(255),
-    similarity DOUBLE,
-    PRIMARY KEY (name1, name2)
-);
-
 # --- !Downs
 
-DROP TABLE CosineSimilarity;
-DROP TABLE TermFrequency;
 DROP TABLE Page;
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,15 +1,22 @@
-# PageLink
+# Default Schema
 
 # --- !Ups
 
-CREATE TABLE Link (
-    src       VARCHAR(255),
-    dst       VARCHAR(255),
-    alias     VARCHAR(255),
-    PRIMARY KEY (src, dst)
+CREATE TABLE TermFrequency (
+    name       VARCHAR(255),
+    term       VARCHAR(255),
+    frequency  INTEGER,
+    PRIMARY KEY (name, term)
 );
 
+CREATE TABLE CosineSimilarity (
+    name1 VARCHAR(255),
+    name2 VARCHAR(255),
+    similarity DOUBLE,
+    PRIMARY KEY (name1, name2)
+);
 
 # --- !Downs
 
-DROP TABLE Link;
+DROP TABLE CosineSimilarity;
+DROP TABLE TermFrequency;
