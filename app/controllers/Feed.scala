2241334419bc6a718d265422b95eb000a36c7739
aha00a <aha00a@gmail.com>
2015-10-19 22:34:09 +0900

    implementing atom

--- app/controllers/Feed.scala
+++ app/controllers/Feed.scala
@@ -11,7 +11,44 @@ import play.api.mvc._
 class Feed @Inject()(implicit cacheApi: CacheApi) extends Controller {
   def index = atom
 
-  def atom = rss // TODO
+  def atom = Action { implicit request =>
+    case class Feed(title:String, subtitle:String, linkSelf:String, link:String, id:String, updated:LocalDateTime) {
+      def toXml = <title>{title}</title>
+        <subtitle>{subtitle}</subtitle>
+        <link href={linkSelf} rel="self" />
+        <link href={link} />
+        <id>{id}</id>
+        <updated>{updated}</updated>
+    }
+
+    case class Entry(title:String, link:String, linkAlternate:String, linkEdit:String, id:String, updated:LocalDateTime, summary:String, content:String, author:String) {
+      def toXml = <entry>
+        <title>{title}</title>
+        <link href={link} />
+        <link rel="alternate" type="text/html" href={linkAlternate} />
+        <link rel="edit" href={linkEdit}/>
+        <id>{id}</id>
+        <updated>{updated}</updated>
+        <summary>{summary}</summary>
+        <content type="xhtml">{content}</content>
+        <author>
+          <name>{author}</name>
+          <email>{author}</email>
+        </author>
+      </entry>
+
+    }
+
+    implicit val wikiContext: WikiContext = WikiContext("")
+    val feed = Feed("title", "subtitle", "linkSelf1", "link", "urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6", LocalDateTime.now()) // TODO
+    val entries = Cache.PageList.get().take(15).map(p => Entry(p.name, p.name, p.name, p.name, p.name, p.localDateTime, p.name, p.name, p.author)) // TODO
+    Ok(
+      <feed xmlns="http://www.w3.org/2005/Atom">
+        {feed.toXml}
+        {entries.map(_.toXml)}
+      </feed>
+    )
+  }
 
   def rss = Action { implicit request =>
     case class Channel(title:String, description:String, link:String, lastBuildDate:LocalDateTime, pubDate:LocalDateTime, ttl:Int) {
@@ -41,7 +78,6 @@ class Feed @Inject()(implicit cacheApi: CacheApi) extends Controller {
         {channel.toXml}
         {items.map(_.toXml)}
       </channel>
-    </rss>
-    )
+    </rss>)
   }
 }
