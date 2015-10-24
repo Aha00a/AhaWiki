2d99d85c04572df85cefce61c517c60a2888494e
aha00a <aha00a@gmail.com>
2015-10-24 22:11:50 +0900

    change query to delete only src is deleted

--- app/models/Database.scala
+++ app/models/Database.scala
@@ -202,7 +202,7 @@ object Database {
   }
 
   def pageDeleteWithRelatedData(name:String): Int = DB.withConnection { implicit connection => // TODO: transaction, FK
-    SQL"DELETE FROM Link WHERE src = $name OR dst = $name".executeUpdate()
+    SQL"DELETE FROM Link WHERE src = $name".executeUpdate()
     SQL"DELETE FROM CosineSimilarity WHERE name1 = $name OR name2 = $name".executeUpdate()
     SQL"DELETE FROM TermFrequency WHERE name = $name".executeUpdate()
     SQL"DELETE FROM Page WHERE name = $name".executeUpdate()
