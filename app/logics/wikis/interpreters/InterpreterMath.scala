5eea72c3dd392f1cb80e5fd6e3faf62703524a67
aha00a <aha00a@gmail.com>
2015-11-05 13:20:40 +0900

    change mathjax

--- app/logics/wikis/interpreters/InterpreterMath.scala
+++ app/logics/wikis/interpreters/InterpreterMath.scala
@@ -2,6 +2,6 @@ package logics.wikis.interpreters
 
 object InterpreterMath {
   def interpret(argument: String, body: String): String = {
-    "$$$$" + body + "$$$$"
+    """<div class="mathjax">$$$$""" + body + "$$$$</div>"
   }
 }
