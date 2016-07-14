5587800434fda34ea3e403ef34a46422832d0249
aha00a <aha00a@gmail.com>
2016-07-14 19:38:54 +0900

    fix wiki formattable

--- app/logics/wikis/WikiFormattable.scala
+++ app/logics/wikis/WikiFormattable.scala
@@ -3,7 +3,7 @@ package logics.wikis
 import models.WikiContext
 
 trait WikiFormattable {
-  def apply(body:String)(implicit wikiContext: WikiContext): String = ""
-  def calcLength(body:String)(implicit wikiContext: WikiContext):Long = 0
+  def apply(body:String)(implicit wikiContext: WikiContext): String = body
+  def calcLength(body:String)(implicit wikiContext: WikiContext):Long = body.length
   def extractLink(body:String)(implicit wikiContext: WikiContext):Seq[String] = Seq()
 }
