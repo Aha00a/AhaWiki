dae4e80a824b9cad3b5ecab8d3171220aad720cd
aha00a <aha00a@gmail.com>
2016-07-14 20:46:02 +0900

    make TraitMacro.extrackLink to return Seq[Link]

--- app/logics/wikis/macros/TraitMacro.scala
+++ app/logics/wikis/macros/TraitMacro.scala
@@ -1,9 +1,10 @@
 package logics.wikis.macros
 
+import models.Database.Link
 import models.WikiContext
 
 trait TraitMacro {
   def apply(argument:String)(implicit wikiContext: WikiContext): String = argument
   def calcLength(argument:String)(implicit wikiContext: WikiContext):Long = argument.length
-  def extractLink(argument:String)(implicit wikiContext: WikiContext):Seq[String] = Seq()
+  def extractLink(argument:String)(implicit wikiContext: WikiContext):Seq[Link] = Seq()
 }
