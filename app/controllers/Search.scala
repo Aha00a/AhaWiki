c13edd858049d7a810c6642e9fcec117ed188cd2
KIM, Aha <aha00a@gmail.com>
2018-02-07 20:43:59 +0900

    fix bug of empty parameter

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -8,15 +8,21 @@ import models.{PageContent, WikiContext, Database}
 import models.Database.SearchResult
 import play.api.cache.CacheApi
 import play.api.mvc._
+import com.aha00a.commons.implicits.Implicits._
 
 class Search @Inject() (implicit on:OnApplicationStart, cacheApi: CacheApi) extends Controller {
   def index(q:String) = Action { implicit request =>
     implicit val wikiContext: WikiContext = WikiContext("")
-
-    Ok(views.html.Search.search(q, Database.pageSearch(q)
-      .filter(sr => WikiPermission.isReadable(new PageContent(sr._2)))
-      .map(sr => SearchResult(sr._1, sr._2.split( """(\r\n|\n)+""").filter(_.contains(q)).mkString(" ... "))))
-    )
+    Ok(views.html.Search.search(
+      q,
+      q.toOption.map(
+        Database.pageSearch(_)
+          .filter(sr => WikiPermission.isReadable(PageContent(sr._2)))
+          .map(sr => SearchResult(
+            sr._1,
+            sr._2.split( """(\r\n|\n)+""").filter(_.contains(q)).mkString(" ... ")
+          ))
+      ).getOrElse(Iterator.empty)))
   }
 
 }
