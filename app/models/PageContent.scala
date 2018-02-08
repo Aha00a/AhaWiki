0b5fbdec2b74f6244791b9e0a01e23cde268affb
KIM, Aha <aha00a@gmail.com>
2018-02-08 16:15:27 +0900

    remove warnings

--- app/models/PageContent.scala
+++ app/models/PageContent.scala
@@ -6,25 +6,23 @@ import Implicits._
 case class PageContent(raw: String) {
   private val split: Array[String] = raw.replaceAll("""^\n#!""", "#!").split("""(\n|\r\n)""")
 
-  val directives = split.toIterator.takeWhile(_.startsWith("#!")).map(_.substring(2)).toSeq
-  val content = split.toIterator.dropWhile(_.startsWith("#!")).mkString("\n")
+  val directives: Seq[String] = split.toIterator.takeWhile(_.startsWith("#!")).map(_.substring(2)).toSeq
+  val content: String = split.toIterator.dropWhile(_.startsWith("#!")).mkString("\n")
 
-  val read = extractDirective("read")
-  val write = extractDirective("write")
-  val redirect = extractDirective("redirect")
+  val read: Option[String] = extractDirective("read")
+  val write: Option[String] = extractDirective("write")
+  val redirect: Option[String] = extractDirective("redirect")
 
   def extractDirective(shebang:String): Option[String] = {
     directives.find(_.startsWith(shebang)).map(_.substring(shebang.length).trim)
   }
 
-  val shebang = directives
+  val shebang: Seq[String] = directives
     .filterNot(_.startsWith("read"))
     .filterNot(_.startsWith("write"))
     .filterNot(_.startsWith("redirect")).flatMap(_.split("""\s+"""))
 
-  val interpreter = shebang.headOption
-  val argument = shebang.tailSafe()
-
-
+  val interpreter: Option[String] = shebang.headOption
+  val argument: Seq[String] = shebang.tailSafe()
 }
 
