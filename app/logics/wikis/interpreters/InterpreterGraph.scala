ce396e116525bfaf7dcfe38574a3edaf88d24848
KIM, Aha <aha00a@gmail.com>
2018-02-09 17:43:56 +0900

    remove warnings

--- app/logics/wikis/interpreters/InterpreterGraph.scala
+++ app/logics/wikis/interpreters/InterpreterGraph.scala
@@ -6,9 +6,9 @@ object InterpreterGraph {
   def apply(pageContent: PageContent)(implicit wikiContext: WikiContext): String = {
     pageContent.extractDirective("enableWikiLink") match {
       case Some(s) =>
-        views.html.Wiki.graph(pageContent.content.trim.split("""(\r\n|\n)+""").flatMap(_.split("->").sliding(2).map(_.toArray)), true).toString()
+        views.html.Wiki.graph(pageContent.content.trim.split("""(\r\n|\n)+""").flatMap(_.split("->").sliding(2).map(_.toArray)), enableWikiLink = true).toString()
       case None =>
-        views.html.Wiki.graph(pageContent.content.trim.split("""(\r\n|\n)+""").flatMap(_.split("->").sliding(2).map(_.toArray)), false).toString()
+        views.html.Wiki.graph(pageContent.content.trim.split("""(\r\n|\n)+""").flatMap(_.split("->").sliding(2).map(_.toArray)), enableWikiLink = false).toString()
     }
   }
 }
