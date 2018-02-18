a3b8cb6735a1604cdfbcf6ad9ffdd46c93f3c885
KIM, Aha <aha00a@gmail.com>
2018-02-18 20:03:35 +0900

    display error message as pre

--- app/logics/wikis/macros/MacroError.scala
+++ app/logics/wikis/macros/MacroError.scala
@@ -3,5 +3,5 @@ package logics.wikis.macros
 import models.WikiContext
 
 object MacroError extends TraitMacro {
-  override def apply(argument: String)(implicit wikiContext: WikiContext): String = s"""<div class="error">$argument</div>"""
+  override def apply(argument: String)(implicit wikiContext: WikiContext): String = s"""<pre class="error">$argument</pre>"""
 }
