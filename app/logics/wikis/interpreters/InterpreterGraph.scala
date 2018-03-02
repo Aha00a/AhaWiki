bb5aa185544a90166e9f05577f4458e9e9cdb168
KIM, Aha <aha00a@gmail.com>
2018-03-02 21:04:52 +0900

    fix bug of parse shebang line for InterpreterGraph

--- app/logics/wikis/interpreters/InterpreterGraph.scala
+++ app/logics/wikis/interpreters/InterpreterGraph.scala
@@ -4,11 +4,12 @@ import models.{WikiContext, PageContent}
 
 object InterpreterGraph {
   def apply(pageContent: PageContent)(implicit wikiContext: WikiContext): String = {
-    pageContent.extractDirective("enableWikiLink") match {
-      case Some(s) =>
-        views.html.Wiki.graph(pageContent.content.trim.split("""(\r\n|\n)+""").flatMap(_.split("->").sliding(2).map(_.toArray)), enableWikiLink = true).toString()
-      case None =>
-        views.html.Wiki.graph(pageContent.content.trim.split("""(\r\n|\n)+""").flatMap(_.split("->").sliding(2).map(_.toArray)), enableWikiLink = false).toString()
-    }
+    views.html.Wiki.graph(
+      pageContent.content.trim
+        .split("""(\r\n|\n)+""")
+        .flatMap(_.split("->").sliding(2).map(_.toArray))
+      ,
+      enableWikiLink = pageContent.shebang.contains("enableWikiLink")
+    ).toString()
   }
 }
