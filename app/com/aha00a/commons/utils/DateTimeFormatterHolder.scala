3fd79d251adecd37655c38386b3f286568d29ff6
KIM, Aha <aha00a@gmail.com>
2018-03-13 14:12:16 +0900

    reorder fields

--- app/com/aha00a/commons/utils/DateTimeFormatterHolder.scala
+++ app/com/aha00a/commons/utils/DateTimeFormatterHolder.scala
@@ -3,9 +3,12 @@ package com.aha00a.commons.utils
 import java.time.format.DateTimeFormatter
 
 object DateTimeFormatterHolder {
-  lazy val yearDashMonth: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
-  lazy val isoLocalDateTime: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
   lazy val isoLocalDateTimeWithMillisec: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+  lazy val isoLocalDateTime: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+
   lazy val isoLocalDate: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+  lazy val yearDashMonth: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
+
   lazy val isoLocalTime: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS")
+
 }
