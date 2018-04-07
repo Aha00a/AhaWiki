645c904c9e59c77ef22b647dfcf1fa52a702d0fe
KIM, Aha <aha00a@gmail.com>
2018-04-07 19:01:19 +0900

    remove warnings

--- app/actionCompositions/PostAction.scala
+++ app/actionCompositions/PostAction.scala
@@ -1,12 +1,11 @@
 package actionCompositions
 
 import play.api.mvc._
-import play.filters.csrf._
 
 import scala.concurrent.Future
 
 object PostAction extends ActionBuilder[Request] {
-  def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+  def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
     // authentication code here
     block(request)
   }
