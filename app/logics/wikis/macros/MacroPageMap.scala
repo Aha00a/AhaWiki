53f1a5be78cfa18e752b91890525707a9d9b4cd6
KIM, Aha <aha00a@gmail.com>
2018-04-07 20:28:14 +0900

    fix warning

--- app/logics/wikis/macros/MacroPageMap.scala
+++ app/logics/wikis/macros/MacroPageMap.scala
@@ -3,14 +3,14 @@ package logics.wikis.macros
 import models.{AhaWikiDatabase, WikiContext}
 
 object MacroPageMap extends TraitMacro {
-  override def apply(argument:String)(implicit wikiContext: WikiContext) = {
+  override def apply(argument:String)(implicit wikiContext: WikiContext): String = {
     views.html.Wiki.graph(
       AhaWikiDatabase().linkSelect()
         .filterNot(l => l.src.contains("://"))
         .filterNot(l => l.dst.contains("://"))
         .map(link => Array(link.src, link.dst))
         .toArray,
-      true
+      enableWikiLink = true
     ).toString()
   }
 }
