288a94308516465da30f0e1c85833612e5fdb434
aha00a <aha00a@gmail.com>
2018-04-24 00:46:49 +0900

    refactoring

--- app/logics/wikis/interpreters/InterpreterMath.scala
+++ app/logics/wikis/interpreters/InterpreterMath.scala
@@ -1,7 +1,5 @@
 package logics.wikis.interpreters
 
 object InterpreterMath {
-  def apply(argument: String, body: String): String = {
-    """<span class="mathjax">$___$""" + body + "$___$</span>"
-  }
+  def apply(argument: String, body: String): String = """<span class="mathjax">$___$""" + body + "$___$</span>"
 }
