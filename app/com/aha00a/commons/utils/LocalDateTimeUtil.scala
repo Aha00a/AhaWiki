3160b4b24819630e28c254df1a0e69007534d9bf
aha00a <aha00a@gmail.com>
2019-10-09 17:44:51 +0900

    fix warning

--- app/com/aha00a/commons/utils/LocalDateTimeUtil.scala
+++ app/com/aha00a/commons/utils/LocalDateTimeUtil.scala
@@ -4,8 +4,8 @@ import java.time.{Instant, LocalDateTime, ZoneId}
 import java.util.Date
 
 object LocalDateTimeUtil {
-  def fromEpochSecond(time:Long) = LocalDateTime.ofInstant(Instant.ofEpochSecond(time), ZoneId.systemDefault())
-  def fromEpochMilli(time:Long) = LocalDateTime.ofInstant(Instant.ofEpochMilli(time), ZoneId.systemDefault())
-  def fromEpochNano(time:Long) = fromEpochMilli(time / 1000)
+  def fromEpochSecond(time:Long): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(time), ZoneId.systemDefault())
+  def fromEpochMilli(time:Long): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(time), ZoneId.systemDefault())
+  def fromEpochNano(time:Long): LocalDateTime = fromEpochMilli(time / 1000)
   def convert(date: Date): LocalDateTime = LocalDateTime.ofInstant(date.toInstant, ZoneId.systemDefault())
 }
