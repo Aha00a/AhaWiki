bd692bcfe64af19c8e86f1eb36685e083c0f9fd7
aha00a <aha00a@gmail.com>
2019-10-25 16:13:05 +0900

    fix to support duck typing.

--- app/com/aha00a/commons/utils/Using.scala
+++ app/com/aha00a/commons/utils/Using.scala
@@ -3,7 +3,7 @@ package com.aha00a.commons.utils
 import java.io.Closeable
 
 object Using {
-  def apply[TCloseable <: Closeable, TResult](resource: TCloseable)(operation: TCloseable => TResult): TResult = {
+  def apply[TCloseable <: { def close(): Unit }, TResult](resource: TCloseable)(operation: TCloseable => TResult): TResult = {
     try {
       operation(resource)
     } finally {
