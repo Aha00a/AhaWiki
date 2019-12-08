0fa346d3030224cc2773c9270751cddf0c2a3bc5
aha00a <aha00a@gmail.com>
2019-12-08 15:57:15 +0900

    fix warning

--- app/logics/wikis/interpreters/InterpreterMap.scala
+++ app/logics/wikis/interpreters/InterpreterMap.scala
@@ -1,19 +1,14 @@
 package logics.wikis.interpreters
 
 import java.io.StringReader
-import java.util
 
 import com.aha00a.commons.utils.Using
-import logics.{AhaWikiCache, ApplicationConf}
 import logics.wikis.interpreters.InterpreterTable.convert
+import logics.{AhaWikiCache, ApplicationConf}
 import models.{LatLng, PageContent, WikiContext}
 import org.supercsv.io.CsvListReader
 import org.supercsv.prefs.CsvPreference
-import play.api.{Configuration, Logger}
-import play.api.libs.ws.WSResponse
-
-import scala.collection.mutable
-import scala.concurrent.Future
+import play.api.Configuration
 
 object InterpreterMap {
   case class Location(name:String, address:String, rest:Seq[String])(implicit wikiContext: WikiContext) {
@@ -22,9 +17,7 @@ object InterpreterMap {
 
   def apply(pageContent: PageContent)(implicit wikiContext: WikiContext): String = {
     Using(new CsvListReader(new StringReader(pageContent.content), CsvPreference.TAB_PREFERENCE)) { listReader =>
-      
-
-      val rowColumnData: Seq[Seq[String]] = convert(listReader);
+      val rowColumnData: Seq[Seq[String]] = convert(listReader)
       val head: Seq[String] = rowColumnData.head
       val tail: Seq[Seq[String]] = rowColumnData.tail
 
@@ -32,9 +25,10 @@ object InterpreterMap {
       val seqFieldIndex: Seq[Int] = seqFields.map(head.indexOf)
       val seqIndexRest: Seq[Int] = head.zipWithIndex.filterNot(v => seqFields.contains(v._1)).map(_._2)
 
+      //noinspection ZeroIndexToHead
       val locations: Seq[Location] = tail.map(row => Location(row(seqFieldIndex(0)), row(seqFieldIndex(1)), seqIndexRest.map(v => row(v))))
 
-      implicit val configuration: Configuration = wikiContext.configuration;
+      implicit val configuration: Configuration = wikiContext.configuration
       val resultMap = views.html.Wiki.map(ApplicationConf().AhaWiki.google.credentials.api.MapsJavaScriptAPI.key(), locations).toString()
       val resultTable = InterpreterTable.interpret(PageContent("#!Table tsv 1 tablesorter\n" + pageContent.content))
       resultMap + resultTable
