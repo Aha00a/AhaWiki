a59a94b189d6b57a1b61da94fbaf978c3024a1b8
aha00a <aha00a@gmail.com>
2019-12-10 00:21:35 +0900

    refactoring

--- app/com/aha00a/commons/utils/ShebangUtil.scala
+++ app/com/aha00a/commons/utils/ShebangUtil.scala
@@ -1,15 +1,6 @@
 package com.aha00a.commons.utils
 
 object ShebangUtil {
-  def has(s: String):Boolean = {
-    s.startsWith("#!")
-  }
-
-  def addWhenNotExist(s:String, shebang:String):String = {
-    if(has(s.trim)) {
-      s
-    } else {
-      "#!" + shebang + "\n" + s
-    }
-  }
+  def has(s: String):Boolean = s.startsWith("#!")
+  def addWhenNotExist(s:String, shebang:String):String = if(has(s.trim)) s else "#!" + shebang + "\n" + s
 }
