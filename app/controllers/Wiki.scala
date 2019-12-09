624f733182c1d27df05a1c7ddd9523651cdb0a71
aha00a <aha00a@gmail.com>
2019-12-09 22:45:07 +0900

    add TODO

--- app/controllers/Wiki.scala
+++ app/controllers/Wiki.scala
@@ -31,6 +31,7 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext}
 import scala.util.matching.Regex
 
+// TODO: Why???? Singleton?
 @Singleton
 class Wiki @Inject()(implicit
                      cacheApi: CacheApi,
