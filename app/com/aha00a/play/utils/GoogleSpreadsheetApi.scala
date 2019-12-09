46275266c729abaf3145bb2807929eca5b673266
aha00a <aha00a@gmail.com>
2019-12-10 00:38:56 +0900

    refactoring

--- app/com/aha00a/play/utils/GoogleSpreadsheetApi.scala
+++ app/com/aha00a/play/utils/GoogleSpreadsheetApi.scala
@@ -7,15 +7,9 @@ import play.api.libs.ws.WSClient
 import scala.concurrent.{ExecutionContext, Future}
 
 object GoogleSpreadsheetApi {
-  def readSpreadSheet(id: String, sheetName: String)(implicit configuration:Configuration, wSClient: WSClient, executionContext: ExecutionContext): Future[Seq[Seq[String]]] = {
-    wSClient
-      .url(s"https://sheets.googleapis.com/v4/spreadsheets/$id/values/$sheetName")
-      .withQueryString(
-        "key" -> ApplicationConf().AhaWiki.google.credentials.api.GoogleSheetsAPI.key()
-      )
-      .get()
-      .map(r => {
-        (r.json \ "values").as[Seq[Seq[String]]]
-      })
-  }
+  def readSpreadSheet(id: String, sheetName: String)(implicit configuration:Configuration, wSClient: WSClient, executionContext: ExecutionContext): Future[Seq[Seq[String]]] = wSClient
+    .url(s"https://sheets.googleapis.com/v4/spreadsheets/$id/values/$sheetName")
+    .withQueryString("key" -> ApplicationConf().AhaWiki.google.credentials.api.GoogleSheetsAPI.key())
+    .get()
+    .map(r => (r.json \ "values").as[Seq[Seq[String]]])
 }
