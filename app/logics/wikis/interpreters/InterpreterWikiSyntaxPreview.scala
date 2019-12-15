b27e04d06735bbc846956a1a0d7402748a2aa544
aha00a <aha00a@gmail.com>
2019-12-15 14:17:37 +0900

    add thead

--- app/logics/wikis/interpreters/InterpreterWikiSyntaxPreview.scala
+++ app/logics/wikis/interpreters/InterpreterWikiSyntaxPreview.scala
@@ -9,19 +9,37 @@ object InterpreterWikiSyntaxPreview {
     val argument = pageContent.argument.mkString(" ")
     val body = pageContent.content
     if (argument == "") {
-      """<table class="wikiSyntax"><tr><td class="raw">""" +
-        Interpreters.interpret("#!text\n" + body) +
-        """</td><td class="preview">""" +
-        Interpreters.interpret("#!wiki\n" + body) +
-        """</td></tr></table>"""
+      s"""<table class="wikiSyntax">
+         |    <thead>
+         |        <tr>
+         |            <th>Raw</th>
+         |            <th>Preview</th>
+         |        </tr>
+         |    </thead>
+         |    <tbody>
+         |        <tr>
+         |            <td class="raw">${Interpreters.interpret("#!text\n" + body)}</td>
+         |            <td class="preview">${Interpreters.interpret("#!wiki\n" + body)}</td>
+         |        </tr>
+         |    </tbody>
+         |</table>""".stripMargin
     }
     else
     {
-      """<table class="wikiSyntax"><tr><td class="raw">""" +
-        Interpreters.interpret(s"#!text\n[[[#!$argument\n" + body + "\n]]]") +
-        """</td><td class="preview">""" +
-        Interpreters.interpret(s"#!$argument\n" + body) +
-        """</td></tr></table>"""
+      s"""<table class="wikiSyntax">
+         |    <thead>
+         |        <tr>
+         |            <th>Row</th>
+         |            <th>Preview</th>
+         |        </tr>
+         |    </thead>
+         |    <tbody>
+         |        <tr>
+         |            <td class="raw">${Interpreters.interpret(s"#!text\n[[[#!$argument\n" + body + "\n]]]")}</td>
+         |            <td class="preview">${Interpreters.interpret(s"#!$argument\n" + body)}</td>
+         |        </tr>
+         |    </tbody>
+         |</table>""".stripMargin
     }
   }
 }
