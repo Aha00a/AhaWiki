386fb2187582ca80d8a43674eb5a7993345e83fa
aha00a <aha00a@gmail.com>
2019-12-16 15:59:09 +0900

    add noise because same address

--- app/models/LatLng.scala
+++ app/models/LatLng.scala
@@ -1,3 +1,9 @@
 package models
 
-case class LatLng(lat: Double, lng: Double)
+import scala.util.Random
+
+case class LatLng(lat: Double, lng: Double) {
+  def noise(): Double = (Random.nextDouble() - 0.5) / 20000
+  def latWithNoise(): Double = lat + noise()
+  def lngWithNoise(): Double = lng + noise()
+}
