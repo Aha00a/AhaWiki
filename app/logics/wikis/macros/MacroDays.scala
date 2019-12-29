bae109c1942d7c4274f8ff6383f3d37fa40f47d6
aha00a <aha00a@gmail.com>
2019-12-29 16:53:44 +0900

    extract method

--- app/logics/wikis/macros/MacroDays.scala
+++ app/logics/wikis/macros/MacroDays.scala
@@ -13,8 +13,8 @@ object MacroDays extends TraitMacro {
   override def apply(argument: String)(implicit wikiContext: WikiContext): String = argument match {
     case "" | null => apply(wikiContext.name)
     case "-" => apply(wikiContext.name + ",-")
-    case regexIncr(y, m) => new InterpreterWiki()((1 to YearMonth.of(y.toInt, m.toInt).lengthOfMonth()).map(d => f" * [$y-${m.toInt}%02d-$d%02d]").mkString("\n"))
-    case regexDecr(y, m) => new InterpreterWiki()((1 to YearMonth.of(y.toInt, m.toInt).lengthOfMonth()).reverse.map(d => f" * [$y-${m.toInt}%02d-$d%02d]").mkString("\n"))
+    case regexIncr(y, m) => new InterpreterWiki()(getSeqDays(y, m).map(d => toAsteriskLinkYmdString(y, m, d)).mkString("\n"))
+    case regexDecr(y, m) => new InterpreterWiki()(getSeqDays(y, m).reverse.map(d => toAsteriskLinkYmdString(y, m, d)).mkString("\n"))
     case _ => MacroError(s"Argument Error - [[$name($argument)]]")
   }
 
@@ -24,8 +24,20 @@ object MacroDays extends TraitMacro {
   override def extractLink(body: String)(implicit wikiContext: WikiContext): Seq[String] = body match {
     case "" | null => extractLink(wikiContext.name)
     case "-" => extractLink(wikiContext.name + ",-")
-    case regexIncr(y, m) => (1 to YearMonth.of(y.toInt, m.toInt).lengthOfMonth()).map(d => f"$y-${m.toInt}%02d-$d%02d")
-    case regexDecr(y, m) => (1 to YearMonth.of(y.toInt, m.toInt).lengthOfMonth()).reverse.map(d => f"$y-${m.toInt}%02d-$d%02d")
+    case regexIncr(y, m) => getSeqDays(y, m).map(d => toYmdString(y, m, d))
+    case regexDecr(y, m) => getSeqDays(y, m).reverse.map(d => toYmdString(y, m, d))
     case _ => Seq()
+  }
+
+  private def getSeqDays(y: String, m: String): Seq[Int] = {
+    1 to YearMonth.of(y.toInt, m.toInt).lengthOfMonth()
+  }
+
+  private def toAsteriskLinkYmdString(y: String, m: String, d: Int) = {
+    f" * [$y-${m.toInt}%02d-$d%02d]"
+  }
+
+  private def toYmdString(y: String, m: String, d: Int) = {
+    f"$y-${m.toInt}%02d-$d%02d"
   }
 }
