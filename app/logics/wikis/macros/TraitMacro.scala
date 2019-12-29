b2f3b8016176aa88c8d4ff0b8688a7379cd66acf
aha00a <aha00a@gmail.com>
2019-12-29 10:53:21 +0900

    add TODO

--- app/logics/wikis/macros/TraitMacro.scala
+++ app/logics/wikis/macros/TraitMacro.scala
@@ -7,7 +7,7 @@ trait TraitMacro {
   val name: String = getClass.getSimpleName.replaceAll("^Macro", "").replaceAll("""\$$""", "")
   def apply(argument:String)(implicit wikiContext: WikiContext): String = argument
   def calcLength(argument:String)(implicit wikiContext: WikiContext):Long = argument.length
-  def extractLink(argument:String)(implicit wikiContext: WikiContext):Seq[String] = Seq()
+  def extractLink(argument:String)(implicit wikiContext: WikiContext):Seq[String] = Seq() // TODO fix to return Seq[Link]
   def extractLinkExistsOnly(argument:String)(implicit wikiContext: WikiContext):Seq[String] = extractLink(argument).filter(existsInPageName)
   def existsInPageName(implicit wikiContext: WikiContext): String => Boolean = AhaWikiCache.PageNameSet.get()(wikiContext.cacheApi, wikiContext.database).contains
 }
