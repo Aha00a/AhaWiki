104d1fb6ceed9c8d535999deadf5cd3acccca07b
aha00a <aha00a@gmail.com>
2019-12-29 15:11:39 +0900

    fix warning

--- app/logics/wikis/macros/MacroDays.scala
+++ app/logics/wikis/macros/MacroDays.scala
@@ -5,9 +5,11 @@ import java.time.YearMonth
 import logics.wikis.interpreters.InterpreterWiki
 import models.WikiContext
 
+import scala.util.matching.Regex
+
 object MacroDays extends TraitMacro {
-  val regexIncr = """^(\d{4})-(\d{2})$""".r
-  val regexDecr = """^(\d{4})-(\d{2}),-$""".r
+  val regexIncr: Regex = """^(\d{4})-(\d{2})$""".r
+  val regexDecr: Regex = """^(\d{4})-(\d{2}),-$""".r
   override def apply(argument: String)(implicit wikiContext: WikiContext): String = argument match {
     case "" | null => apply(wikiContext.name)
     case "-" => apply(wikiContext.name + ",-")
@@ -18,6 +20,7 @@ object MacroDays extends TraitMacro {
 
   override def calcLength(body: String)(implicit wikiContext: WikiContext): Long = extractLink(body).mkString("\n").length
 
+  @scala.annotation.tailrec
   override def extractLink(body: String)(implicit wikiContext: WikiContext): Seq[String] = body match {
     case "" | null => extractLink(wikiContext.name)
     case "-" => extractLink(wikiContext.name + ",-")
