c610b880cf7b38d1f8f85345ce905094bfc6f070
aha00a <aha00a@gmail.com>
2020-01-04 07:05:52 +0900

    fix error

--- app/logics/wikis/macros/MacroPercentLinkTitle.scala
+++ app/logics/wikis/macros/MacroPercentLinkTitle.scala
@@ -5,7 +5,7 @@ import models.WikiContext
 import scala.util.matching.Regex
 
 object MacroPercentLinkTitle extends TraitMacro {
-  val regex: Regex = """([^,\s]+),\s*([^,]+)(?:,\s*"([^"]+))"""".r
+  val regex: Regex = """([^,\s]+),\s*([^,]+)(?:,\s*"([^"]*))"""".r
   override def apply(argument: String)(implicit wikiContext: WikiContext): String = argument match {
     case regex(percent, link, title) => views.html.Wiki.percentLinkTitle(percent.toDouble, link, title).toString()
   }
