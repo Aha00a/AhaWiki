2cb05d07343c703a04fc6095f7879e7df89d6896
aha00a <aha00a@gmail.com>
2020-01-04 14:18:22 +0900

    implement MacroLinkDate

--- app/logics/wikis/macros/MacroLinkDate.scala
+++ app/logics/wikis/macros/MacroLinkDate.scala
@@ -1,13 +1,31 @@
 package logics.wikis.macros
 
-import com.aha00a.commons.utils.DateTimeUtil
+import java.time.LocalDate
+
+import com.aha00a.commons.utils.{DateTimeFormatterHolder, DateTimeUtil}
 import logics.wikis.interpreters.InterpreterWiki
+import logics.wikis.interpreters.InterpreterWiki.LinkMarkup
 import models.WikiContext
+
+import scala.util.Try
 
 object MacroLinkDate extends TraitMacro {
   override def apply(argument: String)(implicit wikiContext: WikiContext): String = argument match {
     case "" | null => apply(wikiContext.name)
-    case DateTimeUtil.regexIsoLocalDate(y, m, d) => argument // TODO
+    case DateTimeUtil.regexYearDashMonth(y, m) => ""
+    case DateTimeUtil.regexIsoLocalDate(y, m, d) =>
+      val links = Seq(
+        s"${l(s"$y-$m-$d")}",
+        s"${l(s"$y-$m")}-${l(s"----${d}")}",
+        s"${l(y)}-${l(s"--$m-$d")}",
+        s"${l(y)}-${l(s"--$m")}-${l(s"----$d")}",
+        ""
+      ).mkString("<br/>")
+      s"""
+         |<div class="MacroLinkDate">
+         |$links
+         |</div>
+         |""".stripMargin
     case _ => MacroError(s"Argument Error - [[$name($argument)]]")
   }
 
@@ -17,4 +35,7 @@ object MacroLinkDate extends TraitMacro {
     case DateTimeUtil.regexIsoLocalDate(y, m, d) => Seq() // TODO
     case _ => Seq()
   }
+
+  def l(s:String)(implicit wikiContext: WikiContext): String = LinkMarkup(s, s.replaceAllLiterally("--", "")).toHtmlString()
+
 }
