75a066ba77c33aa8012d8e7e0324ef77ee0bcfc7
aha00a <aha00a@gmail.com>
2020-01-04 23:57:32 +0900

    fix test

--- app/controllers/Test.scala
+++ app/controllers/Test.scala
@@ -90,18 +90,18 @@ class Test @Inject()(implicit
 
     assertEquals(MacroMonths("1000"),
       """<ul style="list-style-type: ;">
-        |<li><a href="1000-01" class="missing">1000-01</a></li>
-        |<li><a href="1000-02" class="missing">1000-02</a></li>
-        |<li><a href="1000-03" class="missing">1000-03</a></li>
-        |<li><a href="1000-04" class="missing">1000-04</a></li>
-        |<li><a href="1000-05" class="missing">1000-05</a></li>
-        |<li><a href="1000-06" class="missing">1000-06</a></li>
-        |<li><a href="1000-07" class="missing">1000-07</a></li>
-        |<li><a href="1000-08" class="missing">1000-08</a></li>
-        |<li><a href="1000-09" class="missing">1000-09</a></li>
-        |<li><a href="1000-10" class="missing">1000-10</a></li>
-        |<li><a href="1000-11" class="missing">1000-11</a></li>
-        |<li><a href="1000-12" class="missing">1000-12</a></li>
+        |<li><a href="1000-01">1000-01</a></li>
+        |<li><a href="1000-02">1000-02</a></li>
+        |<li><a href="1000-03">1000-03</a></li>
+        |<li><a href="1000-04">1000-04</a></li>
+        |<li><a href="1000-05">1000-05</a></li>
+        |<li><a href="1000-06">1000-06</a></li>
+        |<li><a href="1000-07">1000-07</a></li>
+        |<li><a href="1000-08">1000-08</a></li>
+        |<li><a href="1000-09">1000-09</a></li>
+        |<li><a href="1000-10">1000-10</a></li>
+        |<li><a href="1000-11">1000-11</a></li>
+        |<li><a href="1000-12">1000-12</a></li>
         |</ul>""".stripMargin)
     assertEquals(MacroMonths.extractLink("1000"), "1000-01,1000-02,1000-03,1000-04,1000-05,1000-06,1000-07,1000-08,1000-09,1000-10,1000-11,1000-12".split(',').toSeq)
 
