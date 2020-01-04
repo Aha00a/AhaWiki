2150a29efa33490388ad4ac9bb25c1841a44b363
aha00a <aha00a@gmail.com>
2020-01-04 21:53:10 +0900

    fix code style

--- app/models/WikiContext.scala
+++ app/models/WikiContext.scala
@@ -9,10 +9,7 @@ import play.api.cache.CacheApi
 import play.api.db.Database
 import play.api.mvc.Request
 
-case class WikiContext(
-                        name: String,
-                        renderingMode: RenderingMode = RenderingMode.Normal
-                      )
+case class WikiContext(name: String, renderingMode: RenderingMode = RenderingMode.Normal)
                       (implicit
                        val request: Request[Any],
                        val cacheApi: CacheApi,
