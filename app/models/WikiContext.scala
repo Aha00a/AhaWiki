9a89dade666613c24a98bcd07819158260fd5db3
aha00a <aha00a@gmail.com>
2020-01-04 21:57:38 +0900

    split

--- app/models/WikiContext.scala
+++ app/models/WikiContext.scala
@@ -9,14 +9,19 @@ import play.api.cache.CacheApi
 import play.api.db.Database
 import play.api.mvc.Request
 
-case class WikiContext(name: String, renderingMode: RenderingMode = RenderingMode.Normal)
-                      (implicit
-                       val request: Request[Any],
-                       val cacheApi: CacheApi,
-                       val database: Database,
-                       val actorAhaWiki: ActorRef,
-                       val configuration: Configuration
-                      ) {
+
+object WikiContext {
+  def apply(name: String, renderingMode: RenderingMode = RenderingMode.Normal)(implicit request: Request[Any], cacheApi: CacheApi, database: Database, actorAhaWiki: ActorRef, configuration: Configuration): WikiContext = new WikiContext(name, renderingMode)(request, cacheApi, database, actorAhaWiki, configuration)
+}
+
+class WikiContext(val name: String, val renderingMode: RenderingMode = RenderingMode.Normal)
+                 (implicit
+                  val request: Request[Any],
+                  val cacheApi: CacheApi,
+                  val database: Database,
+                  val actorAhaWiki: ActorRef,
+                  val configuration: Configuration
+                 ) {
 }
 
 
