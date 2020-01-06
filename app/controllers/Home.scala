3398ec28e8fcbec7b1f52f2cd906e67ce2e8926d
aha00a <aha00a@gmail.com>
2020-01-07 02:18:06 +0900

    remove encode

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -21,7 +21,7 @@ class Home @Inject() (
   }
 
   def random: Action[AnyContent] = Action { implicit request =>
-    Redirect(routes.Wiki.view(URLEncoder.encode(AhaWikiCache.PageNameSet.get.toSeq.random(), "UTF-8"), 0, "")).flashing(request.flash)
+    Redirect(routes.Wiki.view(AhaWikiCache.PageNameSet.get.toSeq.random(), 0, "")).flashing(request.flash)
   }
 
   def robotsTxt: Action[AnyContent] = Action { implicit request =>
