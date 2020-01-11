da85189a1574445353ce019584ba79634537196f
aha00a <aha00a@gmail.com>
2020-01-12 01:04:38 +0900

    apply permission for pagemap

--- app/logics/wikis/macros/MacroPageMap.scala
+++ app/logics/wikis/macros/MacroPageMap.scala
@@ -6,6 +6,7 @@ object MacroPageMap extends TraitMacro {
   override def apply(argument:String)(implicit wikiContext: WikiContext): String = { wikiContext.database.withConnection { implicit connection =>
     views.html.Wiki.graph(
       AhaWikiQuery().Link.selectAllButNotEmpty()
+        .filter(l => l.and(wikiContext.setPageName.contains))
         .map(link => Array(link.src, link.dst))
         .toArray,
       enableWikiLink = true
