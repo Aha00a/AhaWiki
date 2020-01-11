e3ad5d2ee48a6bf3ac90a9c03fac3a2266dc770b
aha00a <aha00a@gmail.com>
2020-01-12 00:18:47 +0900

    apply permission check for backlinks

--- app/logics/wikis/macros/MacroBacklinks.scala
+++ app/logics/wikis/macros/MacroBacklinks.scala
@@ -1,11 +1,15 @@
 package logics.wikis.macros
 
+import logics.wikis.PageLogic
 import logics.wikis.interpreters.InterpreterWiki
-import models.{AhaWikiQuery, WikiContext}
+import models.{AhaWikiQuery, Link, WikiContext}
 
 object MacroBacklinks extends TraitMacro {
   override def apply(argument:String)(implicit wikiContext: WikiContext): String = { wikiContext.database.withConnection { implicit connection =>
-    InterpreterWiki(AhaWikiQuery().Link.selectDst(wikiContext.name)
+    val listLink: List[Link] = AhaWikiQuery().Link.selectDst(wikiContext.name)
+    val setPageName = PageLogic.getSetPageName()(wikiContext.request, wikiContext.cacheApi, wikiContext.database)
+    val listLinkFiltered = listLink.filter(l => setPageName.contains(l.src))
+    InterpreterWiki(listLinkFiltered
       .map(l => s""" * ["${l.src}"] ${l.alias}""")
       .mkString("\n")
     )
