5d6d7dcf15cecebfe57b91e1b99f3c3431dfae1f
aha00a <aha00a@gmail.com>
2020-01-17 20:54:43 +0900

    refactoring - code style

--- app/logics/wikis/macros/TraitMacro.scala
+++ app/logics/wikis/macros/TraitMacro.scala
@@ -4,6 +4,8 @@ import models.WikiContext
 
 trait TraitMacro {
   val name: String = getClass.getSimpleName.replaceAll("^Macro", "").replaceAll("""\$$""", "")
-  def apply(argument:String)(implicit wikiContext: WikiContext): String = argument
-  def extractLink(argument:String)(implicit wikiContext: WikiContext):Seq[String] = Seq() // TODO fix to return Seq[Link]
+
+  def apply(argument: String)(implicit wikiContext: WikiContext): String = argument
+
+  def extractLink(argument: String)(implicit wikiContext: WikiContext): Seq[String] = Seq() // TODO fix to return Seq[Link]
 }
