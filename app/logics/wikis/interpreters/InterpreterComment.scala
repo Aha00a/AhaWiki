d602fc2a67ec7bf91d7f43a2611c040aaf7ab334
aha00a <aha00a@gmail.com>
2020-01-17 20:46:00 +0900

    make TraitInterpreter.interpret abstract

--- app/logics/wikis/interpreters/InterpreterComment.scala
+++ app/logics/wikis/interpreters/InterpreterComment.scala
@@ -1,3 +1,6 @@
 package logics.wikis.interpreters
+import models.WikiContext
 
-object InterpreterComment extends TraitInterpreter
+object InterpreterComment extends TraitInterpreter {
+  override def interpret(content: String)(implicit wikiContext: WikiContext): String = ""
+}
--- app/logics/wikis/interpreters/TraitInterpreter.scala
+++ app/logics/wikis/interpreters/TraitInterpreter.scala
@@ -5,7 +5,7 @@ import models.{Link, WikiContext}
 trait TraitInterpreter {
   val name: String = getClass.getSimpleName.replaceAll("^Interpreter", "").replaceAll("""\$$""", "")
 
-  def interpret(content: String)(implicit wikiContext: WikiContext): String = ""
+  def interpret(content: String)(implicit wikiContext: WikiContext): String
   def extractLink(content: String)(implicit wikiContext: WikiContext): Seq[Link] = Seq()
 }
 
