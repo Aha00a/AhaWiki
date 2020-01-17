099afe036f0b5ce0967a351fec4769261af26f4a
aha00a <aha00a@gmail.com>
2020-01-17 19:50:25 +0900

    refactoring - apply TraitInterpreter to InterpreterMarkdown

--- app/logics/wikis/interpreters/InterpreterMarkdown.scala
+++ app/logics/wikis/interpreters/InterpreterMarkdown.scala
@@ -1,9 +1,14 @@
 package logics.wikis.interpreters
-import models.{PageContent, WikiContext}
+import models.{Link, PageContent, WikiContext}
 
 object InterpreterMarkdown extends TraitInterpreter {
   override def interpret(content: String)(implicit wikiContext: WikiContext): String = {
     val pageContent: PageContent = PageContent(content)
     com.github.rjeschke.txtmark.Processor.process(pageContent.content)
+  }
+
+  override def extractLink(content: String)(implicit wikiContext: WikiContext): Seq[Link] = {
+    // TODO:
+    super.extractLink(content)
   }
 }
