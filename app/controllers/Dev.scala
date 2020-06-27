8c300ce24558eac31c7b7afa9ff30d7e07881c9d
aha00a <aha00a@gmail.com>
2020-06-27 13:49:33 +0900

    update parboiled version and fix error

--- app/controllers/Dev.scala
+++ app/controllers/Dev.scala
@@ -2,7 +2,7 @@ package controllers
 
 import akka.actor.{ActorRef, ActorSystem}
 import com.aha00a.play.Implicits._
-import javax.inject.{Inject, Named}
+import javax.inject.Inject
 import logics.wikis.interpreters.InterpreterVim
 import play.api.cache.SyncCacheApi
 import play.api.mvc._
@@ -13,7 +13,7 @@ class Dev @Inject()(
                      syncCacheApi: SyncCacheApi,
                      system: ActorSystem,
                      database:play.api.db.Database,
-                     @Named("db-actor") actorAhaWiki: ActorRef
+                     @javax.inject.Named("db-actor") actorAhaWiki: ActorRef
                    ) extends BaseController {
   def deleteVimCache(md5:String): Action[AnyContent] = Action { implicit request =>
     val result = Redirect(request.refererOrRoot)
