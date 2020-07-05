af5868e75ba98b423ceecf40352c7e304516b566
aha00a <aha00a@gmail.com>
2020-07-05 14:53:31 +0900

    remove unused code

--- app/com/aha00a/play/utils/CookieUtil.scala
+++
@@ -1,9 +0,0 @@
-package com.aha00a.play.utils
-
-import play.api.mvc.Request
-
-object CookieUtil {
-  def value(request:Request[Any], key:String, default:String):String = {
-    request.cookies.get(key).map(_.value).getOrElse(default)
-  }
-}
