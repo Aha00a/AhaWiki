087d53c6f0c2a422ac0a97316644107ed1b8fce4
aha00a <aha00a@gmail.com>
2020-07-05 15:11:14 +0900

    fix code style

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -14,19 +14,25 @@ class Api @Inject()(
                      implicit val
                      controllerComponents: ControllerComponents,
                      syncCacheApi: SyncCacheApi,
-                     database:Database
+                     database: Database
                    ) extends BaseController {
-  def pageMap: Action[AnyContent] = Action { database.withConnection { implicit connection =>
-    import models.tables.Link
-    val listLink = Random.shuffle(Link.selectAllButNotEmpty()).take(10)
+  def pageMap: Action[AnyContent] = Action {
+    database.withConnection { implicit connection =>
+      import models.tables.Link
+      val listLink = Random.shuffle(Link.selectAllButNotEmpty()).take(10)
 
-    Ok(Json.toJson(Map(
-      "links" -> listLink
-        .map(l => Map("src" -> JsString(l.src), "dst" -> JsString(l.dst), "alias" -> JsString(l.alias))))
-    ))
-  }}
+      Ok(Json.toJson(Map(
+        "links" -> listLink.map(l => Map(
+          "src" -> JsString(l.src),
+          "dst" -> JsString(l.dst),
+          "alias" -> JsString(l.alias)
+        ))
+      )))
+    }
+  }
 
   def pageNames: Action[AnyContent] = Action { implicit request =>
     Ok(Json.toJson(PageLogic.getListPageByPermission().map(_.name)))
   }
 }
+
