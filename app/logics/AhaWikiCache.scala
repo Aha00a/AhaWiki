bd7b6df658cac28fc7684e407ec05c39710298da
aha00a <aha00a@gmail.com>
2020-07-05 13:51:12 +0900

    reformat code

--- app/logics/AhaWikiCache.scala
+++ app/logics/AhaWikiCache.scala
@@ -12,8 +12,10 @@ import play.api.db.Database
 import scala.concurrent.duration._
 
 object AhaWikiCache extends Logging {
+
   trait CacheEntity {
     val key: String = getClass.getName
+
     def invalidate()(implicit wikiContext: WikiContext): Unit = {
       logger.info(s"Invalidate Cache: $key")
       wikiContext.syncCacheApi.remove(key)
@@ -24,7 +26,7 @@ object AhaWikiCache extends Logging {
 
     import models.tables.PageWithoutContentWithSize
 
-    def get()(implicit syncCacheApi: SyncCacheApi, database:Database): List[PageWithoutContentWithSize] = syncCacheApi.getOrElseUpdate(key, 60.minutes) {
+    def get()(implicit syncCacheApi: SyncCacheApi, database: Database): List[PageWithoutContentWithSize] = syncCacheApi.getOrElseUpdate(key, 60.minutes) {
       database.withConnection { implicit connection =>
         models.tables.Page.pageSelectPageList()
       }
@@ -32,28 +34,36 @@ object AhaWikiCache extends Logging {
   }
 
   object Header extends CacheEntity {
-    def get()(implicit wikiContext: WikiContext): String = wikiContext.syncCacheApi.getOrElseUpdate(key, 60.minutes) { wikiContext.database.withConnection { implicit connection =>
-      Interpreters.toHtmlString(models.tables.Page.selectLastRevision(".header").map(_.content).getOrElse(""))
-    }}
+    def get()(implicit wikiContext: WikiContext): String = wikiContext.syncCacheApi.getOrElseUpdate(key, 60.minutes) {
+      wikiContext.database.withConnection { implicit connection =>
+        Interpreters.toHtmlString(models.tables.Page.selectLastRevision(".header").map(_.content).getOrElse(""))
+      }
+    }
   }
 
   object Footer extends CacheEntity {
-    def get()(implicit wikiContext: WikiContext): String = wikiContext.syncCacheApi.getOrElseUpdate(key, 60.minutes) { wikiContext.database.withConnection { implicit connection =>
-      Interpreters.toHtmlString(models.tables.Page.selectLastRevision(".footer").map(_.content).getOrElse(""))
-    }}
+    def get()(implicit wikiContext: WikiContext): String = wikiContext.syncCacheApi.getOrElseUpdate(key, 60.minutes) {
+      wikiContext.database.withConnection { implicit connection =>
+        Interpreters.toHtmlString(models.tables.Page.selectLastRevision(".footer").map(_.content).getOrElse(""))
+      }
+    }
   }
 
   object Config extends CacheEntity {
-    def get()(implicit syncCacheApi: SyncCacheApi, database:Database): String = syncCacheApi.getOrElseUpdate(key, 60.minutes) { database.withConnection { implicit connection =>
-      models.tables.Page.selectLastRevision(".config").map(_.content).getOrElse("")
-    }}
+    def get()(implicit syncCacheApi: SyncCacheApi, database: Database): String = syncCacheApi.getOrElseUpdate(key, 60.minutes) {
+      database.withConnection { implicit connection =>
+        models.tables.Page.selectLastRevision(".config").map(_.content).getOrElse("")
+      }
+    }
   }
 
 
   object AddressToLatLng {
     def key(address: String): String = "Addr" + address
+
     def set(address: String, latLng: LatLng)(implicit syncCacheApi: SyncCacheApi): Unit = syncCacheApi.set(key(address), latLng, 365 days)
-    def get(address:String)(implicit syncCacheApi: SyncCacheApi, actorAhaWiki: ActorRef, database: Database): LatLng = {
+
+    def get(address: String)(implicit syncCacheApi: SyncCacheApi, actorAhaWiki: ActorRef, database: Database): LatLng = {
       syncCacheApi.get[LatLng](key(address)) match {
         case Some(latLng) => latLng
         case _ =>
@@ -79,7 +89,9 @@ object AhaWikiCache extends Logging {
 
   object Distance {
     def key(src: String, dst: String): String = "Addr" + src + dst
+
     def set(src: String, dst: String, meters: Int)(implicit syncCacheApi: SyncCacheApi): Unit = syncCacheApi.set(key(src, dst), meters, 365 days)
+
     def get(src: String, dst: String)(implicit syncCacheApi: SyncCacheApi, actorAhaWiki: ActorRef, database: Database): Int = {
       syncCacheApi.get[Int](key(src, dst)) match {
         case Some(meters) => meters
@@ -100,4 +112,5 @@ object AhaWikiCache extends Logging {
       }
     }
   }
+
 }
