e9b9238db578c821dad03fdc35b12284f6e348c2
aha00a <aha00a@gmail.com>
2020-07-06 21:14:24 +0900

    fix warning

--- app/com/aha00a/supercsv/SupercsvUtil.scala
+++ app/com/aha00a/supercsv/SupercsvUtil.scala
@@ -3,7 +3,6 @@ package com.aha00a.supercsv
 import java.io.StringWriter
 
 import com.aha00a.commons.utils.Using
-import com.aha00a.supercsv.Implicits._
 import org.supercsv.io.CsvListWriter
 import org.supercsv.prefs.CsvPreference
 
