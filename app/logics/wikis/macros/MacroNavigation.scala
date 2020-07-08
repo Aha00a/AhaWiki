c41c4650a16c14b6248317ec0b1d5390c54a53bc
aha00a <aha00a@gmail.com>
2020-07-08 17:49:08 +0900

    apply permission check

--- app/logics/wikis/macros/MacroNavigation.scala
+++ app/logics/wikis/macros/MacroNavigation.scala
@@ -4,10 +4,11 @@ import logics.wikis.interpreters.InterpreterWiki
 import models.WikiContext
 
 object MacroNavigation extends TraitMacro{
-  override def toHtmlString(argument:String)(implicit wikiContext: WikiContext) = {
+  override def toHtmlString(argument:String)(implicit wikiContext: WikiContext): String = {
     InterpreterWiki.replaceLink(
       "RecentChanges,TitleIndex,PageList,PageMap".split(",")
         .filter(_ != wikiContext.name)
+        .filter(wikiContext.setPageNameByPermission.contains)
         .map(name => s"[$name]")
         .mkString(", ")
     )
