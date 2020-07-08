644b725081584b9bdeda90d29a2795255db1a909
aha00a <aha00a@gmail.com>
2020-07-08 18:24:49 +0900

    remove name check

--- app/logics/wikis/macros/MacroNavigation.scala
+++ app/logics/wikis/macros/MacroNavigation.scala
@@ -7,7 +7,6 @@ object MacroNavigation extends TraitMacro{
   override def toHtmlString(argument:String)(implicit wikiContext: WikiContext): String = {
     InterpreterWiki.replaceLink(
       "RecentChanges,TitleIndex,PageList,PageMap".split(",")
-        .filter(_ != wikiContext.name)
         .filter(wikiContext.setPageNameByPermission.contains)
         .map(name => s"[$name]")
         .mkString(", ")
