e8231ee6cf93e06720d6366d8eafa5ce9ea38491
aha00a <aha00a@gmail.com>
2020-07-12 15:44:52 +0900

    refactor tests

--- app/controllers/Test.scala
+++ app/controllers/Test.scala
@@ -65,8 +65,6 @@ class Test @Inject()(implicit val
     testInterpreterWiki()
 
 
-    testParboiled()
-
     CliTest.run(testUtil)
     Ok("Ok.")
   }
@@ -314,39 +312,6 @@ class Test @Inject()(implicit val
 
 
 
-  def testParboiled(): Unit = {
-    import org.parboiled2._
-
-    import scala.util.Try
-
-    class Calculator(val input: ParserInput) extends Parser {
-      def InputLine = rule { Expression ~ EOI }
-
-      def Expression: Rule1[Int] = rule {
-        Term ~ zeroOrMore(
-          '+' ~ Term ~> ((_: Int) + _)
-            | '-' ~ Term ~> ((_: Int) - _))
-      }
-
-      def Term = rule {
-        Factor ~ zeroOrMore(
-          '*' ~ Factor ~> ((_: Int) * _)
-            | '/' ~ Factor ~> ((_: Int) / _))
-      }
-
-      def Factor = rule { Number | Parens }
-
-      def Parens = rule { '(' ~ Expression ~ ')' }
-
-      def Number = rule { capture(Digits) ~> (_.toInt) }
-
-      def Digits = rule { oneOrMore(CharPredicate.Digit) }
-    }
-
-    val triedInt: Try[Int] = new Calculator("1+1").InputLine.run()
-    assertEquals(triedInt.isSuccess, true)
-    assertEquals(triedInt.get, 2)
-  }
 }
 
 
