32268244198d65f19c3eb9de7d424944926246ad
aha00a <aha00a@gmail.com>
2020-07-22 16:12:12 +0900

    action toggle

---
+++ public/js/js.js
@@ -0,0 +1,35 @@
+// https://stackoverflow.com/a/28132596/3751968
+location.params = function(params) {
+    var obj = {}, i, parts, len, key, value;
+
+    if (typeof params === 'string') {
+        value = location.search.match(new RegExp('[?&]' + params + '=?([^&]*)[&#$]?'));
+        return value ? value[1] : undefined;
+    }
+
+    var _params = location.search.substr(1).split('&');
+
+    for (i = 0, len = _params.length; i < len; i++) {
+        parts = _params[i].split('=');
+        if (! parts[0]) {continue;}
+        obj[parts[0]] = parts[1] || true;
+    }
+
+    if (typeof params !== 'object') {return obj;}
+
+    for (key in params) {
+        value = params[key];
+        if (typeof value === 'undefined') {
+            delete obj[key];
+        } else {
+            obj[key] = value;
+        }
+    }
+
+    parts = [];
+    for (key in obj) {
+        parts.push(key + (obj[key] === true ? '' : '=' + obj[key]));
+    }
+
+    location.search = parts.join('&');
+};
