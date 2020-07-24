9d3b792967361628102160c808b11e687ee07bd6
aha00a <aha00a@gmail.com>
2020-07-24 19:05:18 +0900

    apply permission check

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -1,11 +1,17 @@
 package controllers
 
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
 import javax.inject._
 import logics.wikis.PageLogic
+import play.api.Configuration
+import play.api.Environment
 import play.api.cache.SyncCacheApi
 import play.api.db.Database
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 
+import scala.concurrent.ExecutionContext
 import scala.util.Random
 
 
@@ -13,7 +19,13 @@ class Api @Inject()(
                      implicit val
                      controllerComponents: ControllerComponents,
                      syncCacheApi: SyncCacheApi,
-                     database: Database
+                     actorSystem: ActorSystem,
+                     database: Database,
+                     environment: Environment,
+                     @Named("db-actor") actorAhaWiki: ActorRef,
+                     configuration: Configuration,
+                     wsClient: WSClient,
+                     executionContext: ExecutionContext
                    ) extends BaseController {
   import io.circe.generic.auto._
   import io.circe.syntax._
@@ -36,7 +48,9 @@ class Api @Inject()(
 
   def links(name: String): Action[AnyContent] = Action { implicit request =>
     database.withConnection { implicit connection =>
-      val seqLink: Seq[Link] = Link.select(name)
+      import models.WikiContext
+      implicit val wikiContext: WikiContext = WikiContext(name)
+      val seqLink: Seq[Link] = Link.select(name).filter(_.and(wikiContext.pageCanSee))
       Ok(seqLink.asJson)
     }
   }
