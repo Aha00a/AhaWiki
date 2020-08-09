367fbe6fb521cd0f9a93fc5a1f4f5450a96a73ef
aha00a <aha00a@gmail.com>
2020-08-09 16:04:53 +0900

    fix indent

--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -14,20 +14,20 @@ import play.api.mvc._
 import scala.concurrent.ExecutionContext
 
 class Admin @Inject()(
-                     implicit val
-controllerComponents: ControllerComponents,
-                     syncCacheApi: SyncCacheApi,
-                     actorSystem: ActorSystem,
-                     database: Database,
-                     environment: Environment,
-                     @Named("db-actor") actorAhaWiki: ActorRef,
-                     configuration: Configuration,
-                     wsClient: WSClient,
-                     executionContext: ExecutionContext
-                   ) extends BaseController {
+  implicit val
+  controllerComponents: ControllerComponents,
+  syncCacheApi: SyncCacheApi,
+  actorSystem: ActorSystem,
+  database: Database,
+  environment: Environment,
+  @Named("db-actor") actorAhaWiki: ActorRef,
+  configuration: Configuration,
+  wsClient: WSClient,
+  executionContext: ExecutionContext
+) extends BaseController {
   def index(): Action[AnyContent] = Action { implicit request =>
     import com.aha00a.play.PlayContext
-    
+
     implicit val playContext: PlayContext = PlayContext()
     Ok(views.html.Admin.index())
   }
