63cbabcd9031d0a07bbeccc47de50ca1841b0d91
aha00a <aha00a@gmail.com>
2020-08-12 02:57:33 +0900

    fix test

--- test/logics/wikis/macros/MacroBrSpec.scala
+++ test/logics/wikis/macros/MacroBrSpec.scala
@@ -5,9 +5,12 @@ import org.scalatest.freespec.AnyFreeSpec
 class MacroBrSpec extends AnyFreeSpec {
   "name" in {
     import logics.AhaWikiInjects
+    import logics.wikis.RenderingMode
     import models.WikiContext
+    import models.WikiContext.Provider
 
-    implicit val wikiContext: WikiContext = WikiContext("UnitTest")(null, AhaWikiInjects()(null, null, null, null))
+    implicit val ahaWikiInjects: AhaWikiInjects = AhaWikiInjects()(null, null, null, null)
+    implicit val wikiContext: WikiContext = new WikiContext(Seq("UnitTest"), RenderingMode.Normal)(ahaWikiInjects, Provider.empty)
     val empty = ""
     val dummy = "dummy"
     assert(MacroBr.toHtmlString(empty) === "<br/>")
