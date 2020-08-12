408ec1699bc243562140e0ea11419fb373996ab6
aha00a <aha00a@gmail.com>
2020-08-12 17:46:57 +0900

    add test

--- test/logics/SchemaOrgSpec.scala
+++ test/logics/SchemaOrgSpec.scala
@@ -8,4 +8,10 @@ class SchemaOrgSpec extends AnyFreeSpec {
     assert(SchemaOrg.mapClass.size == 818)
     assert(SchemaOrg.mapProperty.size == 1278)
   }
+  "SchemaType" - {
+    "toXmlSpan" in {
+      val schemaType: SchemaOrg.SchemaType = SchemaOrg.mapAll("Movie")
+      assert(schemaType.toXmlSpan().toString() === """<span title="A movie." class="">Movie </span>""")
+    }
+  }
 }
