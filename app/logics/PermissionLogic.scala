3d8f6b7a85f500c499954ea1aa30c8bba9d1ab6c
aha00a <aha00a@gmail.com>
2020-10-04 20:40:34 +0900

    ensure priority

--- app/logics/PermissionLogic.scala
+++ app/logics/PermissionLogic.scala
@@ -4,7 +4,8 @@ import com.aha00a.commons.Implicits._
 import models.tables.Permission
 
 class PermissionLogic(seqPermission: Seq[Permission]) {
+  val seq: Seq[Permission] = seqPermission.sortBy(-_.priority)
   def check(target: String, actor: String, action: Int): Boolean = {
-    seqPermission.find(_.matches(target, actor)).exists(_.permitted(action))
+    seq.find(_.matches(target, actor)).exists(_.permitted(action))
   }
 }
