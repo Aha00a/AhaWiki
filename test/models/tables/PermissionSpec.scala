907a0379ee82039f9243966e9327df6eeccfd7d7
aha00a <aha00a@gmail.com>
2020-10-11 18:55:56 +0900

    add test cases

--- test/models/tables/PermissionSpec.scala
+++ test/models/tables/PermissionSpec.scala
@@ -6,6 +6,7 @@ class PermissionSpec extends AnyFreeSpec {
   "" in {
     val permissionAnyAnyRead = Permission(1, 1, "", "", Permission.read)
     val permissionAnyGmailRead = Permission(1, 1, "", "gmail.com", Permission.read)
+    val permissionAnyAha00aGmailAdmin = Permission(1, 1, "", "aha00a@gmail.com", Permission.admin)
 
     assert(permissionAnyAnyRead.matches("", ""))
     assert(permissionAnyAnyRead.matches("", "asdf"))
@@ -18,5 +19,11 @@ class PermissionSpec extends AnyFreeSpec {
     assert(!permissionAnyGmailRead.matches("", "aha00b@gmail.com"))
     assert(!permissionAnyGmailRead.matches("", "aha00a@aharise.com"))
     assert(permissionAnyGmailRead.actorLevel === 2)
+
+    assert(!permissionAnyAha00aGmailAdmin.matches("", ""))
+    assert(permissionAnyAha00aGmailAdmin.matches("", "aha00a@gmail.com"))
+    assert(!permissionAnyAha00aGmailAdmin.matches("", "aha00b@gmail.com"))
+    assert(!permissionAnyAha00aGmailAdmin.matches("", "aha00a@aharise.com"))
+    assert(permissionAnyAha00aGmailAdmin.actorLevel === 3)
   }
 }
