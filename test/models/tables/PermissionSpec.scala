1666d32fe0f0253efd2b4157a4a83ab73a7314d1
aha00a <aha00a@gmail.com>
2020-10-11 18:39:58 +0900

    add test

--- test/models/tables/PermissionSpec.scala
+++ test/models/tables/PermissionSpec.scala
@@ -6,5 +6,8 @@ class PermissionSpec extends AnyFreeSpec {
   "" in {
     assert(Permission(1, 1, "", "", Permission.read).matches("", ""))
     assert(!Permission(1, 1, "", "gmail.com", Permission.read).matches("", ""))
+    assert(!Permission(1, 1, "", "gmail.com", Permission.read).matches("", "aha00a@gmail.com"))
+    assert(!Permission(1, 1, "", "gmail.com", Permission.read).matches("", "aha00b@gmail.com"))
+    assert(!Permission(1, 1, "", "gmail.com", Permission.read).matches("", "aha00a@aharise.com"))
   }
 }
