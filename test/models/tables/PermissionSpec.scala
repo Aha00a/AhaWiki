bbf63dada40bf5cb09b582765918d6c47a083384
aha00a <aha00a@gmail.com>
2020-10-11 18:35:51 +0900

    add test

--- test/models/tables/PermissionSpec.scala
+++ test/models/tables/PermissionSpec.scala
@@ -5,5 +5,6 @@ import org.scalatest.freespec.AnyFreeSpec
 class PermissionSpec extends AnyFreeSpec {
   "" in {
     assert(Permission(1, 1, "", "", Permission.read).matches("", ""))
+    assert(!Permission(1, 1, "", "gmail.com", Permission.read).matches("", ""))
   }
 }
