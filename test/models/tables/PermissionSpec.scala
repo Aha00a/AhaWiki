1ceee939719665462b9487382b37ca0da5d58c67
aha00a <aha00a@gmail.com>
2020-10-11 18:51:12 +0900

    refactoring

--- test/models/tables/PermissionSpec.scala
+++ test/models/tables/PermissionSpec.scala
@@ -5,13 +5,14 @@ import org.scalatest.freespec.AnyFreeSpec
 class PermissionSpec extends AnyFreeSpec {
   "" in {
     val permissionAnyAnyRead = Permission(1, 1, "", "", Permission.read)
+    val permissionAnyGmailRead = Permission(1, 1, "", "gmail.com", Permission.read)
+
     assert(permissionAnyAnyRead.matches("", ""))
     assert(permissionAnyAnyRead.matches("", "asdf"))
     assert(permissionAnyAnyRead.matches("asdf", ""))
     assert(permissionAnyAnyRead.matches("asdf", "asdf"))
     assert(permissionAnyAnyRead.actorLevel === 1)
 
-    val permissionAnyGmailRead = Permission(1, 1, "", "gmail.com", Permission.read)
     assert(!permissionAnyGmailRead.matches("", ""))
     assert(!permissionAnyGmailRead.matches("", "aha00a@gmail.com"))
     assert(!permissionAnyGmailRead.matches("", "aha00b@gmail.com"))
