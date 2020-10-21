b84525d5d4f0b8a487cf27d05eb1468461a93b63
aha00a <aha00a@gmail.com>
2020-10-22 02:23:58 +0900

    fix formatting

--- app/logics/wikis/macros/MacroBacklinks.scala
+++ app/logics/wikis/macros/MacroBacklinks.scala
@@ -4,15 +4,20 @@ import logics.wikis.interpreters.InterpreterWiki
 import models.WikiContext
 
 object MacroBacklinks extends TraitMacro {
-  override def toHtmlString(argument:String)(implicit wikiContext: WikiContext): String = { wikiContext.database.withConnection { implicit connection =>
-    import com.aha00a.commons.Implicits._
-    import models.tables.Link
-    val listLink: List[Link] = Link.selectDst(wikiContext.name)
-    val listLinkFiltered = listLink.filter(l => l.and(wikiContext.pageCanSee))
-    if(0 == listLinkFiltered.length) {
-      ""
-    } else {
-      InterpreterWiki.toHtmlString(listLinkFiltered.map(l => s"""["${l.src}" ${l.src}${if(l.alias.isNullOrEmpty) "" else "(" + l.alias + ")"}]""").mkString(", "))
+  override def toHtmlString(argument: String)(implicit wikiContext: WikiContext): String = {
+    wikiContext.database.withConnection { implicit connection =>
+      import com.aha00a.commons.Implicits._
+      import models.tables.Link
+      val listLink: List[Link] = Link.selectDst(wikiContext.name)
+      val listLinkFiltered = listLink.filter(l => l.and(wikiContext.pageCanSee))
+      if (0 == listLinkFiltered.length) {
+        ""
+      } else {
+        InterpreterWiki.toHtmlString(listLinkFiltered
+          .map(l => s"""["${l.src}" ${l.src}${if (l.alias.isNullOrEmpty) "" else "(" + l.alias + ")"}]""")
+          .mkString(", ")
+        )
+      }
     }
-  }}
+  }
 }
