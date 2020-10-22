5ba2314316977b4d51c32ff05f8f9620cc4748c7
aha00a <aha00a@gmail.com>
2020-10-22 15:00:11 +0900

    add test

--- test/com/aha00a/commons/utils/LetterUtilSpec.scala
+++ test/com/aha00a/commons/utils/LetterUtilSpec.scala
@@ -5,6 +5,7 @@ import org.scalatest.freespec.AnyFreeSpec
 class LetterUtilSpec extends AnyFreeSpec {
   "firstLetterForIndex" - {
     import com.aha00a.commons.utils.LetterUtil.firstLetterForIndex
+
     "roman" in {
       assert(firstLetterForIndex("A") === 'A')
       assert(firstLetterForIndex("a") === 'A')
@@ -14,6 +15,11 @@ class LetterUtilSpec extends AnyFreeSpec {
       assert(firstLetterForIndex("Ā") === 'A')
       assert(firstLetterForIndex("ȧ") === 'A')
       assert(firstLetterForIndex("ǒ") === 'O')
+
+      assert(
+        "ÈÉÊËÛÙÏÎÀÂÔèéêëûùïîàâôÇçÃãÕõçÇáéíóúýÁÉÍÓÚÝàèìòùÀÈÌÒÙãõñäëïöüÿÄËÏÖÜÃÕÑâêîôûÂÊÎÔÛ".map(c => firstLetterForIndex(c + "postfix")).mkString ===
+        "EEEEUUIIAAOeeeeuuiiaaoCcAaOocCaeiouyAEIOUYaeiouAEIOUaonaeiouyAEIOUAONaeiouAEIOU".toUpperCase()
+      )
     }
 
     "ko" in {
