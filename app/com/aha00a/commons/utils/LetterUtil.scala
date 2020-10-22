946cd5945b6da8ce1e766dda4802e798971d30d8
aha00a <aha00a@gmail.com>
2020-10-22 16:59:05 +0900

    refactoring

--- app/com/aha00a/commons/utils/LetterUtil.scala
+++ app/com/aha00a/commons/utils/LetterUtil.scala
@@ -4,12 +4,9 @@ object LetterUtil {
   def firstLetterForIndex(s: String): Char = {
     import java.text.Normalizer
 
-    import scala.util.matching.Regex
-
     val c = s.charAt(0)
-    val regexHangul: Regex = """[가-힣]""".r
     c match {
-      case regexHangul() =>
+      case Hangul.regexHangul() =>
         Hangul(Hangul(c).index초성, 0, 0).c
       case _ =>
         Normalizer.normalize(c.toString, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}", "").charAt(0).toUpper
