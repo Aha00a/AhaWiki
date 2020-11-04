9852f8a89db436de17685371eeea89e2f03217b8
aha00a <aha00a@gmail.com>
2020-11-05 02:51:30 +0900

    fix warnings

--- app/logics/wikis/macros/MacroEmbed.scala
+++ app/logics/wikis/macros/MacroEmbed.scala
@@ -3,7 +3,10 @@ package logics.wikis.macros
 import models.WikiContext
 
 object MacroEmbed extends TraitMacro {
-  val regexYouTube = """(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})""".r
+
+  import scala.util.matching.Regex
+
+  val regexYouTube: Regex = """(?:youtube(?:-nocookie)?\.com/(?:[^/\n\s]+/\S+/|(?:v|e(?:mbed)?)/|\S*?[?&]v=)|youtu\.be/)([a-zA-Z0-9_-]{11})""".r
   override def toHtmlString(argument: String)(implicit wikiContext: WikiContext): String = {
     regexYouTube.findFirstMatchIn(argument).map(m => m.group(1)).map(views.html.Wiki.youtube(_).toString())
       .getOrElse(MacroError.toHtmlString(s"Argument Error - [[$name($argument)]]"))
