d000d146b8038e2c9628ff463f7ba6fa447ed07f
aha00a <aha00a@gmail.com>
2020-11-06 04:14:33 +0900

    fix link bug

--- app/logics/wikis/macros/MacroPageList.scala
+++ app/logics/wikis/macros/MacroPageList.scala
@@ -5,18 +5,24 @@ import logics.wikis.interpreters.InterpreterTable
 import models.WikiContext
 
 object MacroPageList extends TraitMacro {
-  override def toHtmlString(argument: String)(implicit wikiContext: WikiContext): String = InterpreterTable.toHtmlString(
-    "#!Table tsv 1 tablesorter\nName\tDate\tSize\tRevision\tAuthor\tRemote Address\tComment\n" +
-      wikiContext.listPageByPermission.map { t =>
-      Seq(
-        s"'''[${t.name}]'''",
-        s"${t.localDateTime.toIsoLocalDateTimeString}",
-        s"${t.size}",
-        s"""[[Html(<a href="${t.name}?action=diff&after=${t.revision}">${t.revision}</a>)]]""",
-        s"[${t.author}]",
-        s"${t.remoteAddress}",
-        s"${t.comment}"
-      ).mkString("\t")
-    }.mkString("\n")
-  )
+  override def toHtmlString(argument: String)(implicit wikiContext: WikiContext): String = {
+    import com.aha00a.supercsv.SupercsvUtil
+    val th = Seq("Name", "Date", "Size", "Revision", "Author", "Remote Address", "Comment").mkString("\t")
+    val table: Seq[Seq[String]] = wikiContext.listPageByPermission.map(t => Seq(
+      s"""'''["${t.name}"]'''""",
+      s"${t.localDateTime.toIsoLocalDateTimeString}",
+      s"${t.size}",
+      s"""[[Html(<a href="${t.name}?action=diff&after=${t.revision}">${t.revision}</a>)]]""",
+      s"[${t.author}]",
+      s"${t.remoteAddress}",
+      s"${t.comment}"
+    ))
+
+    InterpreterTable.toHtmlString(
+      s"""#!Table tsv 1 tablesorter
+         |$th
+         |${SupercsvUtil.toTsvString(table)}
+         |""".stripMargin
+    )
+  }
 }
