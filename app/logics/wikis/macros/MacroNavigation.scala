4c2cc8a26862c179ea01c1f24a564c7e3202cd3b
aha00a <aha00a@gmail.com>
2020-11-20 19:20:30 +0900

    implement statistics

--- app/logics/wikis/macros/MacroNavigation.scala
+++ app/logics/wikis/macros/MacroNavigation.scala
@@ -6,7 +6,7 @@ import models.WikiContext
 object MacroNavigation extends TraitMacro{
   override def toHtmlString(argument:String)(implicit wikiContext: WikiContext): String = {
     InterpreterWiki.replaceLink(
-      "RecentChanges,TitleIndex,PageList,PageMap".split(",")
+      "RecentChanges,TitleIndex,PageList,PageMap,WikiStatistics".split(",")
         .filter(wikiContext.setPageNameByPermission.contains)
         .map(name => s"[$name]")
         .mkString(", ")
