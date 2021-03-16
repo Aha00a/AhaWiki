538ae7edf44f38860e10ebe316be4e5d20fc895a
aha00a <aha00a@gmail.com>
2021-03-16 15:03:44 +0900

    fix error

--- app/logics/wikis/macros/MacroCopyable.scala
+++ app/logics/wikis/macros/MacroCopyable.scala
@@ -1,9 +1,10 @@
 package logics.wikis.macros
 
-import models.ContextWikiPage
+import models.WikiContext
 
 object MacroCopyable extends TraitMacro {
-  override def toHtmlString(argument: String)(implicit wikiContext: ContextWikiPage): String = {
+
+  override def toHtmlString(argument: String)(implicit wikiContext: WikiContext): String = {
     <input value={argument} class="MacroCopyable" readonly="readonly"/>.toString()
   }
 }
