d08c27d1c05eddc0a2fad9b819697e6dc7151f9e
aha00a <aha00a@gmail.com>
2021-03-18 01:36:51 +0900

    introduce ``something`` syntax

--- app/logics/wikis/ExtractConvertInjectBackQuote.scala
+++ app/logics/wikis/ExtractConvertInjectBackQuote.scala
@@ -5,16 +5,24 @@ import models.WikiContext
 
 class ExtractConvertInjectBackQuote() extends ExtractConvertInject {
   override def extract(s: String): String = {
-    val regex = """`(.+?)`""".r
-    regex.replaceAllIn(s, _ match {
-      case regex(body) =>
+    val regexDoubleBackquote = """``(.+?)``""".r
+    val s1 = regexDoubleBackquote.replaceAllIn(s, _ match {
+      case regexDoubleBackquote(body) =>
         val uniqueKey = getUniqueKey
-        arrayBuffer += uniqueKey -> body
+        arrayBuffer += uniqueKey -> <input value={body} class="MacroCopyable" readonly="readonly"/>.toString
+        uniqueKey
+      case _ => "error"
+    })
+    val regexSingleBackquote = """`(.+?)`""".r
+    regexSingleBackquote.replaceAllIn(s1, _ match {
+      case regexSingleBackquote(body) =>
+        val uniqueKey = getUniqueKey
+        arrayBuffer += uniqueKey -> s"<code>${body.escapeHtml()}</code>"
         uniqueKey
       case _ => "error"
     })
   }
 
 
-  override def convert(s: String)(implicit wikiContext: WikiContext): String = s"<code>${s.escapeHtml()}</code>"
+  override def convert(s: String)(implicit wikiContext: WikiContext): String = s
 }
