c4c5d955b8a2bff6ceeefcfa24c9a459192aeef5
aha00a <aha00a@gmail.com>
2021-03-28 04:20:03 +0900

    add table Referer

---
+++ conf/evolutions/default/19.sql
@@ -0,0 +1,21 @@
+
+# --- !Ups
+
+create table Referer
+(
+    seq int auto_increment primary key,
+    site int not null,
+    name varchar(255) not null,
+    created datetime default CURRENT_TIMESTAMP not null,
+    url varchar(2048) not null,
+    constraint Referer_Page_site_name_fk foreign key (site, name) references Page (site, name)
+);
+
+
+
+
+# --- !Downs
+
+drop table Referer;
+
+
