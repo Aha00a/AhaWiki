e11faa94d9955cf317eade928f6cd492c87c72e1
aha00a <aha00a@gmail.com>
2021-08-19 01:36:45 +0900

    fix timeout

--- app/com/aha00a/play/utils/GoogleSpreadsheetApi.scala
+++ app/com/aha00a/play/utils/GoogleSpreadsheetApi.scala
@@ -11,7 +11,7 @@ object GoogleSpreadsheetApi {
   def readSpreadSheet(key: String, id: String, sheetName: String)(implicit configuration:Configuration, wSClient: WSClient, executionContext: ExecutionContext): Future[Seq[Seq[String]]] = wSClient
     .url(s"https://sheets.googleapis.com/v4/spreadsheets/$id/values/'$sheetName'!A1:Z1000")
     .withQueryStringParameters("key" -> key)
-    .withRequestTimeout(10 seconds)
+    .withRequestTimeout(60 seconds)
     .get()
     .map(r => (r.json \ "values").as[Seq[Seq[String]]])
 }
