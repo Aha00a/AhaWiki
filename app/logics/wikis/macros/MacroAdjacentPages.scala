16c9e74694da922bac9ca09bd0df397ab0fbcfc6
aha00a <aha00a@gmail.com>
2024-02-17 15:15:23 +0900

    optimize imports

--- app/logics/wikis/macros/MacroAdjacentPages.scala
+++ app/logics/wikis/macros/MacroAdjacentPages.scala
@@ -1,10 +1,6 @@
 package logics.wikis.macros
 
-import com.aha00a.commons.Implicits._
-import logics.wikis.interpreters.InterpreterWiki
 import models.ContextWikiPage
-
-import java.sql.Connection
 
 object MacroAdjacentPages extends TraitMacro {
   override def toHtmlString(argument:String)(implicit wikiContext: ContextWikiPage): String = {
