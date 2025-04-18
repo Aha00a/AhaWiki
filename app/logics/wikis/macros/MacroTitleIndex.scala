06575c8aa082e555c68b9480ea97d74b31675f9c
aha00a <aha00a@gmail.com>
2025-04-18 11:46:40 +0900

    remove unused connection

--- app/logics/wikis/macros/MacroTitleIndex.scala
+++ app/logics/wikis/macros/MacroTitleIndex.scala
@@ -1,16 +1,16 @@
 package logics.wikis.macros
 
+import com.aha00a.commons.utils.LetterUtil
 import logics.wikis.interpreters.InterpreterWiki
 import models.ContextWikiPage
 
 object MacroTitleIndex extends TraitMacro {
-  override def toHtmlString(argument:String)(implicit wikiContext: ContextWikiPage): String = { wikiContext.database.withConnection { implicit connection =>
+  override def toHtmlString(argument: String)(implicit wikiContext: ContextWikiPage): String = {
     val listPageName: Seq[String] = wikiContext.seqPageNameByPermission
-    InterpreterWiki.toHtmlString {
-      import com.aha00a.commons.utils.LetterUtil
+    InterpreterWiki.toHtmlString(
       listPageName.groupBy(LetterUtil.firstLetterForIndex).toList.sortBy(_._1).map {
         case (k, v) => s"== $k\n" + v.map(s => s""" 1. ["$s"]""").mkString("\n")
       }.mkString("\n")
-    }}
+    )
   }
 }
