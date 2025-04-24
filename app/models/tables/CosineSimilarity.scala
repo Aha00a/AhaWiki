e615cce28bce972193ff0a1dd0f976f087c7c40e
aha00a <aha00a@gmail.com>
2025-04-24 22:19:38 +0900

    add another query for performance check

--- app/models/tables/CosineSimilarity.scala
+++ app/models/tables/CosineSimilarity.scala
@@ -1,12 +1,13 @@
 package models.tables
 
-import java.sql.Connection
-
-import anorm._
 import anorm.SqlParser.double
 import anorm.SqlParser.flatten
 import anorm.SqlParser.str
+import anorm._
 import models.tables
+
+import java.sql.Connection
+import scala.util.Random
 
 case class CosineSimilarity(name1: String, name2: String, similarity: Double) {
   def and(a: String => Boolean):Boolean = a(name1) && a(name2)
@@ -20,8 +21,8 @@ object CosineSimilarity {
 
   def recalc(name: String)(implicit connection: Connection, site: Site): Int = {
     SQL"""DELETE FROM CosineSimilarity WHERE site = ${site.seq} AND (name1 = $name OR name2 = $name)""".executeUpdate()
-
-    SQL"""
+    if(Random.nextBoolean()) {
+      SQL"""
 REPLACE INTO CosineSimilarity (site, name1, name2, similarity)
 SELECT *
     FROM (
@@ -68,6 +69,50 @@ SELECT *
     ) CS1
     WHERE similarity > 0.3 AND name1 != name2
       """.executeUpdate()
+    } else {
+      SQL"""
+REPLACE INTO CosineSimilarity (site, name1, name2, similarity)
+WITH
+Norm AS (
+    SELECT
+        name,
+        SQRT(SUM(frequency * frequency)) AS norm
+    FROM TermFrequency
+    WHERE site = ${site.seq}
+    GROUP BY name
+),
+NormTarget AS (
+    SELECT
+        SQRT(SUM(frequency * frequency)) AS norm
+    FROM TermFrequency
+    WHERE site = ${site.seq} AND name = $name
+),
+DotProduct AS (
+    SELECT
+        TF1.name AS name1,
+        SUM(TF1.frequency * TF2.frequency) AS dot
+    FROM TermFrequency TF1
+    JOIN TermFrequency TF2
+        ON TF1.term = TF2.term
+    WHERE
+        TF1.site = ${site.seq} AND
+        TF2.site = ${site.seq} AND
+        TF2.name = $name
+    GROUP BY TF1.name
+)
+SELECT
+    ${site.seq} AS site,
+    DP.name1,
+    $name AS name2,
+    IFNULL(DP.dot / (N.norm * NT.norm), 0) AS similarity
+FROM DotProduct DP
+JOIN Norm N ON DP.name1 = N.name
+CROSS JOIN NormTarget NT
+WHERE
+    DP.name1 != $name
+    AND IFNULL(DP.dot / (N.norm * NT.norm), 0) > 0.3
+      """.executeUpdate()
+    }
 
     SQL"""
 REPLACE INTO CosineSimilarity (site, name1, name2, similarity)
