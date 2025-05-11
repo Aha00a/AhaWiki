42c0e7d9554c7c0a274bdb0e3982ffd389bae851
aha00a <aha00a@gmail.com>
2025-05-11 19:11:29 +0900

    reformat code

--- app/com/aha00a/play/utils/GoogleSpreadsheetApi.scala
+++ app/com/aha00a/play/utils/GoogleSpreadsheetApi.scala
@@ -8,10 +8,18 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
 object GoogleSpreadsheetApi {
-  def readSpreadSheet(key: String, id: String, sheetName: String)(implicit configuration:Configuration, wSClient: WSClient, executionContext: ExecutionContext): Future[Seq[Seq[String]]] = wSClient
-    .url(s"https://sheets.googleapis.com/v4/spreadsheets/$id/values/'$sheetName'!A1:Z1000")
-    .withQueryStringParameters("key" -> key)
-    .withRequestTimeout(60 seconds)
-    .get()
-    .map(r => (r.json \ "values").as[Seq[Seq[String]]])
+  def readSpreadSheet(
+    key: String,
+    id: String,
+    sheetName: String
+  )(
+    implicit configuration:Configuration,
+    wSClient: WSClient,
+    executionContext: ExecutionContext
+  ): Future[Seq[Seq[String]]] = wSClient
+      .url(s"https://sheets.googleapis.com/v4/spreadsheets/$id/values/'$sheetName'!A1:Z1000")
+      .withQueryStringParameters("key" -> key)
+      .withRequestTimeout(60 seconds)
+      .get()
+      .map(r => (r.json \ "values").as[Seq[Seq[String]]])
 }
