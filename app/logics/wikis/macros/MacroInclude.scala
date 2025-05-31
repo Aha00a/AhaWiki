9fac033f2b6e82a877c45d29b731a879e3d28b77
aha00a <aha00a@gmail.com>
2025-06-01 05:46:14 +0900

    optimize imports in MacroInclude.scala

--- app/logics/wikis/macros/MacroInclude.scala
+++ app/logics/wikis/macros/MacroInclude.scala
@@ -2,18 +2,16 @@ package logics.wikis.macros
 
 import logics.wikis.WikiPermission
 import logics.wikis.interpreters.Interpreters
-import models.PageContent
 import models.ContextWikiPage
+import models.PageContent
+import models.RequestWrapper
+import models.tables.Site
+import play.api.db.Database
 
 object MacroInclude extends TraitMacro {
   override def toHtmlString(argument: String)(implicit wikiContext: ContextWikiPage): String = doApply(argument, s => s)
   def doApply(argument: String, preprocessor:String => String)(implicit wikiContext: ContextWikiPage): String = { wikiContext.database.withConnection { implicit connection =>
-    import models.RequestWrapper
-    import models.tables.Site
-    import play.api.cache.SyncCacheApi
-    import play.api.db.Database
     implicit val provider: RequestWrapper = wikiContext.requestWrapper
-    implicit val database: Database = wikiContext.database
     implicit val site: Site = wikiContext.site
     val pageLastRevision = models.tables.Page.selectLastRevision(argument)
     if (WikiPermission().isReadable(pageLastRevision.map(s => PageContent(s.content)))) {
