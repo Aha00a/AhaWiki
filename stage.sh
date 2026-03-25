5f0db5e92545ecd11bcd94f40dd8e72fe3f43060
aha00a <aha00a@gmail.com>
2026-03-26 02:58:31 +0900

    Revert "add fallback check for sbt in stage.sh script"
    This reverts commit 9a4cc0001e99f7ecfe84f372ee76d1c6e1f0b24c.

--- stage.sh
+++ stage.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-set -eu
 
-if command -v sbt >/dev/null 2>&1; then
-  sbt stage
-elif [ -x "$HOME/sbt/bin/sbt" ]; then
-  "$HOME/sbt/bin/sbt" stage
-else
-  echo "Error: sbt not found. Install sbt or add it to PATH." >&2
-  exit 127
-fi
+~/sbt/bin/sbt stage
+
