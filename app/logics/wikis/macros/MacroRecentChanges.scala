043cc3b7f68685734c3e24a809ace4fbf25783c0
aha00a <aha00a@gmail.com>
2026-04-19 16:59:15 +0900

    add support for filtering minor edits in recent changes macros and history view; update logic and UI accordingly

--- app/logics/wikis/macros/MacroRecentChanges.scala
+++ app/logics/wikis/macros/MacroRecentChanges.scala
@@ -7,29 +7,42 @@ import logics.wikis.interpreters.InterpreterWiki
 import models.ContextWikiPage
 
 object MacroRecentChanges extends TraitMacro {
+  private def shouldIncludeMinorEdit(argument: String): Either[String, Boolean] = {
+    Option(argument).map(_.trim.toLowerCase).filter(_.nonEmpty) match {
+      case None => Right(false)
+      case Some("minor") | Some("include-minor") | Some("include_minor") | Some("includeminor") => Right(true)
+      case Some(other) => Left(other)
+    }
+  }
+
   override def toHtmlString(argument:String)(implicit wikiContext: ContextWikiPage): String = {
-    def desc[T : Ordering]: Ordering[T] = implicitly[Ordering[T]].reverse
-    InterpreterWiki.toHtmlString(
-      wikiContext.seqPageByPermission.groupBy(_.year).toList.sortBy(_._1)(desc).map {
-        case (year, groupedByYear) =>
-          s"== $year\n" +
-            groupedByYear.groupBy(_.yearDashMonth).toList.sortBy(_._1)(desc).map {
-              case (yearMonth, groupedByYearMonth) =>
-                val list = groupedByYearMonth.sortBy(_.dateTime)(desc).map(t => Seq(
-                    s"""'''["${t.name}"]'''""",
-                    s"""["${t.name}?action=diff&after=${t.revision}" ${t.revision}]""",
-                    s"${t.toIsoLocalDateTimeString}",
-                    s"${t.nickname.map(UserPageLogic.wikiMarkup).getOrElse("")} ${IpAddressUtil.mask(t.remoteAddress)}",
-                    s"${if (t.isMinorEdit) "[minor] " else ""}${t.comment}"
-                ))
-                s"""=== $yearMonth
-                   |[[[#!Table tsv 1 tablesorter
-                   |Name	Revision	at	by	comment
-                   |${SupercsvUtil.toTsvString(list)}
-                   |]]]
-                   |""".stripMargin
-            }.mkString("\n")
-      }.mkString("\n")
-    )
+    shouldIncludeMinorEdit(argument) match {
+      case Left(_) => MacroError.toHtmlString(s"Bad argument - [[$name($argument)]]")
+      case Right(includeMinorEdit) =>
+        def desc[T : Ordering]: Ordering[T] = implicitly[Ordering[T]].reverse
+        val pageList = wikiContext.seqPageByPermission.filter(p => includeMinorEdit || !p.isMinorEdit)
+        InterpreterWiki.toHtmlString(
+          pageList.groupBy(_.year).toList.sortBy(_._1)(desc).map {
+            case (year, groupedByYear) =>
+              s"== $year\n" +
+                groupedByYear.groupBy(_.yearDashMonth).toList.sortBy(_._1)(desc).map {
+                  case (yearMonth, groupedByYearMonth) =>
+                    val list = groupedByYearMonth.sortBy(_.dateTime)(desc).map(t => Seq(
+                      s"""'''["${t.name}"]'''""",
+                      s"""["${t.name}?action=diff&after=${t.revision}" ${t.revision}]""",
+                      s"${t.toIsoLocalDateTimeString}",
+                      s"${t.nickname.map(UserPageLogic.wikiMarkup).getOrElse("")} ${IpAddressUtil.mask(t.remoteAddress)}",
+                      s"${if (t.isMinorEdit) "[minor] " else ""}${t.comment}"
+                    ))
+                    s"""=== $yearMonth
+                       |[[[#!Table tsv 1 tablesorter
+                       |Name	Revision	at	by	comment
+                       |${SupercsvUtil.toTsvString(list)}
+                       |]]]
+                       |""".stripMargin
+                }.mkString("\n")
+          }.mkString("\n")
+        )
+    }
   }
 }
--- app/logics/wikis/macros/MacroRecentChangesList.scala
+++ app/logics/wikis/macros/MacroRecentChangesList.scala
@@ -4,21 +4,38 @@ import com.aha00a.commons.utils.IpAddressUtil
 import logics.wikis.UserPageLogic
 import logics.wikis.interpreters.InterpreterWiki
 import models.ContextWikiPage
-import play.api.db.Database
-
-import scala.util.matching.Regex
 
 object MacroRecentChangesList extends TraitMacro {
 
   import models.tables.PageWithoutContentWithSize
 
-  val regexDigits: Regex = """^(\d+)$""".r
+  private val includeMinorEditKeywords = Set("minor", "include-minor", "include_minor", "includeminor")
+
+  private def parseArgument(argument: String): Either[String, (Option[Int], Boolean)] = {
+    val tokens = Option(argument).map(_.trim).filter(_.nonEmpty).toSeq
+      .flatMap(_.split(",").toSeq.map(_.trim).filter(_.nonEmpty))
+      .map(_.toLowerCase)
+    if (tokens.isEmpty) {
+      Right((None, false))
+    } else {
+      val (sizeTokens, optionTokens) = tokens.partition(_.matches("""\d+"""))
+      if (sizeTokens.size > 1) {
+        Left(argument)
+      } else if (optionTokens.forall(includeMinorEditKeywords.contains)) {
+        Right((sizeTokens.headOption.map(_.toInt), optionTokens.nonEmpty))
+      } else {
+        Left(argument)
+      }
+    }
+  }
+
   override def toHtmlString(argument:String)(implicit wikiContext: ContextWikiPage): String = {
     def desc[T : Ordering]: Ordering[T] = implicitly[Ordering[T]].reverse
-    argument match {
-      case "" | null => toHtmlString(wikiContext.seqPageByPermission.sortBy(_.dateTime)(desc))
-      case regexDigits(i) => toHtmlString(wikiContext.seqPageByPermission.sortBy(_.dateTime)(desc).take(i.toInt))
-      case _ => MacroError.toHtmlString(s"Bad argument - [[$name($argument)]]")
+    parseArgument(argument) match {
+      case Left(_) => MacroError.toHtmlString(s"Bad argument - [[$name($argument)]]")
+      case Right((sizeOpt, includeMinorEdit)) =>
+        val filtered = wikiContext.seqPageByPermission.filter(p => includeMinorEdit || !p.isMinorEdit).sortBy(_.dateTime)(desc)
+        toHtmlString(sizeOpt.map(filtered.take).getOrElse(filtered))
     }
   }
 
