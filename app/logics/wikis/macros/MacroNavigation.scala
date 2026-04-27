c4a5fb51e3b0de64b13cf03a48514d6ecd1fa91a
aha00a <aha00a@gmail.com>
2026-04-27 23:44:21 +0900

    update MacroNavigation: adjust schema link formatting for consistency with other navigation links

--- app/logics/wikis/macros/MacroNavigation.scala
+++ app/logics/wikis/macros/MacroNavigation.scala
@@ -7,10 +7,10 @@ import models.ContextWikiPage
 object MacroNavigation extends TraitMacro{
   override def toHtmlString(argument:String)(implicit wikiContext: ContextWikiPage): String = {
     InterpreterWiki.replaceLink(
-      "RecentChanges,TitleIndex,PageList,PageMap,WikiStatistics,schema:Schema".split(",")
+      "RecentChanges,TitleIndex,PageList,PageMap,WikiStatistics".split(",")
         .filter(v => wikiContext.setPageNameByPermission.contains(v) || DefaultPageLogic.isDefined(v))
         .map(name => s"[$name]")
-        .mkString(", ")
+        .mkString(", ") + ", [schema:Schema Schema]"
     )
   }
 }
