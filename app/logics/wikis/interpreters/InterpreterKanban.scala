b748f4ed322ace8ab9aa9e52e2cf7242fdd30b5b
aha00a <aha00a@gmail.com>
2026-05-02 22:19:43 +0900

    implement Kanban

--- app/logics/wikis/interpreters/InterpreterKanban.scala
+++ app/logics/wikis/interpreters/InterpreterKanban.scala
@@ -1,12 +1,14 @@
 package logics.wikis.interpreters
 
-import com.aha00a.commons.Implicits._
-import models.ContextWikiPage
+import models.{ContextWikiPage, PageContent}
 import models.tables.CalculatedLink
 
 object InterpreterKanban extends TraitInterpreter {
   override def toHtmlString(content: String)(implicit wikiContext: ContextWikiPage): String = {
-    s"""<pre class="Interpreter InterpreterKanban" data-kanban-test="TODO">${content.escapeHtml()}</pre>"""
+    val pageContent = PageContent(content)
+    val id = com.aha00a.commons.utils.UuidUtil.newString
+    val shebangLine = pageContent.shebang.mkString(" ")
+    views.html.Wiki.InterpreterKanban(id, pageContent.content, shebangLine).toString()
   }
 
   override def toSeqLink(content: String)(implicit wikiContext: ContextWikiPage): Seq[CalculatedLink] = Seq() // TODO
