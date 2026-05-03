caf1bab617c9e9283e15d6ac24775a34d6d7eac1
aha00a <aha00a@gmail.com>
2026-05-04 08:15:17 +0900

    fix to use cache for user profile image URLs

--- app/logics/wikis/UserPageLogic.scala
+++ app/logics/wikis/UserPageLogic.scala
@@ -1,5 +1,7 @@
 package logics.wikis
 
+import com.aha00a.commons.Implicits._
+
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
@@ -12,15 +14,14 @@ object UserPageLogic {
     "/w/" + URLEncoder.encode(pageName(nickname), StandardCharsets.UTF_8.toString).replace("+", "%20")
 
   private def profileImageUrlByNickname(nickname: String)(implicit wikiContext: models.ContextWikiPage): Option[String] = {
-    wikiContext.database.withConnection { implicit connection =>
-      models.tables.User.selectByNickname(nickname).flatMap(_.profileImageUrl)
-    }
+    implicit val userProfileImageCacheKey: (play.api.db.Database, models.tables.Site, String) = (wikiContext.database, wikiContext.site, nickname)
+    wikiContext.ahaWikiCache.UserProfileImageUrl.get()
   }
 
   def toHtmlString(nickname: String)(implicit wikiContext: models.ContextWikiPage): String =
     profileImageUrlByNickname(nickname)
       .map { imageUrl =>
-        s"""<span class="userInlineProfile"><img src="$imageUrl" alt="$nickname" class="userInlineProfileImage"/><a href="${href(nickname)}">$nickname</a></span>"""
+        s"""<span class="userInlineProfile"><img src="${imageUrl.escapeHtmlAttribute()}" alt="${nickname.escapeHtmlAttribute()}" class="userInlineProfileImage"/><a href="${href(nickname).escapeHtmlAttribute()}">${nickname.escapeHtml()}</a></span>"""
       }
       .getOrElse(logics.wikis.interpreters.InterpreterWiki.inlineToHtmlString(wikiMarkup(nickname)))
 }
