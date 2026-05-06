a939a9954b37b728968c295b48e02bf6fc0821b9
aha00a <aha00a@gmail.com>
2026-05-07 04:17:11 +0900

    fix

--- app/logics/wikis/macros/MacroPageList.scala
+++ app/logics/wikis/macros/MacroPageList.scala
@@ -2,30 +2,45 @@ package logics.wikis.macros
 
 import com.aha00a.commons.Implicits._
 import com.aha00a.commons.utils.IpAddressUtil
+import com.aha00a.commons.utils.UriUtil
 import logics.wikis.UserPageLogic
-import logics.wikis.interpreters.InterpreterTable
 import models.ContextWikiPage
 
 object MacroPageList extends TraitMacro {
   override def isBlock: Boolean = true
-  override def toHtmlString(argument: String)(implicit wikiContext: ContextWikiPage): String = {
-    import com.aha00a.supercsv.SupercsvUtil
-    val th = Seq("Name", "Date", "Size", "Revision", "Author", "Remote Address", "Comment").mkString("\t")
-    val table: Seq[Seq[String]] = wikiContext.seqPageByPermission.map(t => Seq(
-      s"""'''["${t.name}"]'''""",
-      s"${t.localDateTime.toIsoLocalDateTimeString}",
-      s"${t.size}",
-      s"""[[Html(<a href="${t.name}?action=diff&after=${t.revision}">${t.revision}</a>)]]""",
-      s"${t.nickname.map(UserPageLogic.wikiMarkup).getOrElse("")}",
-      s"${IpAddressUtil.mask(t.remoteAddress)}",
-      s"${t.comment}"
-    ))
 
-    InterpreterTable.toHtmlString(
-      s"""#!Table tsv 1 tablesorter
-         |$th
-         |${SupercsvUtil.toTsvString(table)}
-         |""".stripMargin
-    )
+  override def toHtmlString(argument: String)(implicit wikiContext: ContextWikiPage): String = {
+    val sb = new StringBuilder(4096)
+
+    sb.append("<table class=\"tablesorter\"><thead><tr>")
+      .append("<th>Name</th><th>Date</th><th>Size</th><th>Revision</th><th>Author</th><th>Remote Address</th><th>Comment</th>")
+      .append("</tr></thead><tbody>")
+
+    wikiContext.seqPageByPermission.foreach { page =>
+      val pageHref = s"/w/${UriUtil.encodeURIComponent(page.name)}"
+      val diffHref = s"$pageHref?action=diff&after=${page.revision}"
+
+      sb.append("<tr><td><strong><a href=\"")
+        .append(pageHref.escapeHtmlAttribute())
+        .append("\">")
+        .append(page.name.escapeHtml())
+        .append("</a></strong></td><td>")
+        .append(page.localDateTime.toIsoLocalDateTimeString.escapeHtml())
+        .append("</td><td class=\"text-right\">")
+        .append(page.size)
+        .append("</td><td><a href=\"")
+        .append(diffHref.escapeHtmlAttribute())
+        .append("\">")
+        .append(page.revision)
+        .append("</a></td><td>")
+        .append(page.nickname.map(UserPageLogic.wikiMarkup).getOrElse(""))
+        .append("</td><td>")
+        .append(IpAddressUtil.mask(page.remoteAddress).escapeHtml())
+        .append("</td><td>")
+        .append(page.comment.escapeHtml())
+        .append("</td></tr>")
+    }
+
+    sb.append("</tbody></table>").result()
   }
 }
--- public/js/Wiki/pageList.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-    $(".simpleTable").tablesorter();
-});
