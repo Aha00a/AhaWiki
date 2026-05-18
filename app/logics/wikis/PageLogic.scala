a9bd0ca5798f716aad537f232afc0659bfbd75c8
aha00a <aha00a@gmail.com>
2026-05-18 19:51:14 +0900

    remove redundant log

--- app/logics/wikis/PageLogic.scala
+++ app/logics/wikis/PageLogic.scala
@@ -129,7 +129,7 @@ object PageLogic {
     requestWrapper: RequestWrapper,
     logger: Logger,
     site: Site,
-  ): Unit = StopWatch(s"calculate\t${site.name}(${site.seq})\t$name\t\t") {
+  ): Unit = {
     val verbose = false;
     val seqStopWord: Seq[String] = """at in on of by to is the gmail com http https""".stripMargin.split("""\s""").toSeq
 
