23b4b9172fc8c49687803f0abc0895d987c60522
aha00a <aha00a@gmail.com>
2026-05-20 03:58:17 +0900

    optimize

--- app/logics/wikis/interpreters/InterpreterTable.scala
+++ app/logics/wikis/interpreters/InterpreterTable.scala
@@ -18,6 +18,10 @@ object InterpreterTable extends TraitInterpreter {
   val regexShebang: Regex = """([ct]sv)(?:\s+(\d+)(?:\s+(\d+))?)?(?:\s+(.+))?""".r
 
   private val regexCssClassToken: Regex = """^[A-Za-z0-9_-]+$""".r
+  private val regexBlockHeading: Regex = """^(={1,6})(>)?\s+.+""".r
+  private val regexBlockHr: Regex = """^[-=]{4,}$""".r
+  private val regexBlockList: Regex = """^\s+([*-]|\S+\.)\s+.+""".r
+  private val regexBlockHtmlLine: Regex = """^\s*</?(Columns|div|span)(?:\s+[^>]*)?>\s*$""".r
   private val defaultCellValue = ""
 
   case class Shebang(csvPreference:CsvPreference, thRow:Int, thColumn:Int, classes:Option[String]) {
@@ -44,6 +48,39 @@ object InterpreterTable extends TraitInterpreter {
 
   private def normalizeCell(value: String): String = Option(value).getOrElse(defaultCellValue)
 
+  private def canRenderCellInline(value: String): Boolean = {
+    value.nonEmpty &&
+      !value.exists(c => c == '\n' || c == '\r') &&
+      !value.contains("[[") &&
+      !value.contains("`") &&
+      !regexBlockHeading.matches(value) &&
+      !regexBlockHr.matches(value) &&
+      !regexBlockList.matches(value) &&
+      !regexBlockHtmlLine.matches(value)
+  }
+
+  private def cellToHtml(value: String)(implicit wikiContext: ContextWikiPage): String = {
+    val cell = normalizeCell(value)
+    if (cell.isEmpty) {
+      "<div></div>"
+    } else if (canRenderCellInline(cell)) {
+      s"<div><p>${InterpreterWiki.inlineToHtmlString(cell)}</p></div>"
+    } else {
+      InterpreterWiki.toHtmlString(cell)
+    }
+  }
+
+  private def cellToSeqLink(value: String)(implicit wikiContext: ContextWikiPage): Seq[CalculatedLink] = {
+    val cell = normalizeCell(value)
+    if (cell.isEmpty) {
+      Seq()
+    } else if (canRenderCellInline(cell)) {
+      InterpreterWiki.extractLinkMarkup(cell).map(_.toLink(wikiContext.name)).filterNot(_.dst.startsWith("[")).toSeq
+    } else {
+      InterpreterWiki.toSeqLink(cell)
+    }
+  }
+
   def parseShebang(argument:Seq[String]): Option[Shebang] = argument.mkString(" ") match {
     case regexShebang(format, thRow, thColumn, classes) =>
       csvPreferenceFrom(format).map(csvPreference =>
@@ -63,27 +100,32 @@ object InterpreterTable extends TraitInterpreter {
 
   override def toHtmlString(content: String)(implicit wikiContext:ContextWikiPage): String = {
     withShebangAndRows(content) { (shebang, rows) =>
-      val rowColumnData = rows
-        .map(row => row
-          .map(normalizeCell)
-          .map(InterpreterWiki.toHtmlString)
-          .zipWithIndex
-        )
-        .zipWithIndex
-      val (head, body) = rowColumnData.partition(r => r._2 < shebang.thRow)
-      val thead = head
-        .map(_._1
-          .map(col => s"<th>${col._1}</th>")
-          .mkString
-        )
-        .map(s => s"<tr>$s</tr>")
-        .mkString("\n")
-      val tbody = body
-        .map(_._1
-          .map(col => if (col._2 < shebang.thColumn) s"<th>${col._1}</th>" else s"<td>${col._1}</td>")
-          .mkString
-        )
-        .map(s => s"<tr>$s</tr>").mkString("\n")
+      val theadBuilder = new StringBuilder
+      val tbodyBuilder = new StringBuilder
+      val cellHtmlCache = scala.collection.mutable.HashMap.empty[String, String]
+      def cachedCellToHtml(cell: String): String = cellHtmlCache.getOrElseUpdate(normalizeCell(cell), cellToHtml(cell))
+
+      rows.zipWithIndex.foreach { case (row, rowIndex) =>
+        val rowBuilder = new StringBuilder
+        row.zipWithIndex.foreach { case (cell, columnIndex) =>
+          if (rowIndex < shebang.thRow || columnIndex < shebang.thColumn) {
+            rowBuilder.append("<th>").append(cachedCellToHtml(cell)).append("</th>")
+          } else {
+            rowBuilder.append("<td>").append(cachedCellToHtml(cell)).append("</td>")
+          }
+        }
+
+        if (rowIndex < shebang.thRow) {
+          if (theadBuilder.nonEmpty) theadBuilder.append("\n")
+          theadBuilder.append("<tr>").append(rowBuilder).append("</tr>")
+        } else {
+          if (tbodyBuilder.nonEmpty) tbodyBuilder.append("\n")
+          tbodyBuilder.append("<tr>").append(rowBuilder).append("</tr>")
+        }
+      }
+
+      val thead = theadBuilder.toString
+      val tbody = tbodyBuilder.toString
       if(thead.isEmpty)
         s"""<table class="InterpreterTable ${shebang.getClasses}"><tbody>$tbody</tbody></table>"""
       else
@@ -100,7 +142,7 @@ object InterpreterTable extends TraitInterpreter {
 
   override def toSeqLink(content: String)(implicit wikiContext: ContextWikiPage): Seq[CalculatedLink] = {
     withShebangAndRows(content) { (_, rows) =>
-      rows.flatMap(_.map(normalizeCell).flatMap(InterpreterWiki.toSeqLink))
+      rows.flatMap(_.flatMap(cellToSeqLink))
     }.getOrElse(Seq())
   }
 }
