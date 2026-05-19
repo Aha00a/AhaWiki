eadf740d74bff01ef8934af0344c6f84f50ed9ed
aha00a <aha00a@gmail.com>
2026-05-19 21:05:25 +0900

    image fallback

--- app/com/aha00a/tests/unit/WikiMacrosUnit.scala
+++ app/com/aha00a/tests/unit/WikiMacrosUnit.scala
@@ -1,7 +1,7 @@
 package com.aha00a.tests.unit
 
 import com.aha00a.tests.TestUtil
-import logics.wikis.macros.{MacroBr, MacroCalendar, MacroColorCode, MacroUptime, MacroWeekdayName}
+import logics.wikis.macros.{MacroBr, MacroCalendar, MacroColorCode, MacroImage, MacroUptime, MacroWeekdayName}
 import models.ContextWikiPage
 
 object WikiMacrosUnit {
@@ -19,6 +19,10 @@ object WikiMacrosUnit {
     assertEquals(MacroColorCode.toHtmlString(empty), "")
     assertEquals(MacroColorCode.toHtmlString("#fff"), """<span class="MacroColorCode"><spen class="preview" style="background-color: #fff"></spen> #fff</span>""")
     assertEquals(MacroColorCode.toHtmlString("#zzz"), """<div class="error">Argument Error - [[ColorCode(#zzz)]]</div>""")
+    assertEquals(MacroImage.toHtmlString(empty), """<img src="/assets/img/attachmentFallback.svg" alt="/assets/img/attachmentFallback.svg" onerror="this.onerror=null;this.src='/assets/img/attachmentFallback.svg';"/>""")
+    assertEquals(MacroImage.toHtmlString("https://example.com/a.png"), """<img src="https://example.com/a.png" alt="https://example.com/a.png" onerror="this.onerror=null;this.src='/assets/img/attachmentFallback.svg';"/>""")
+    assertEquals(MacroImage.toHtmlString("https://example.com/a.png, 120"), """<img src="https://example.com/a.png" alt="https://example.com/a.png" onerror="this.onerror=null;this.src='/assets/img/attachmentFallback.svg';" style="width: 120px"/>""")
+    assertEquals(MacroImage.toHtmlString("https://example.com/a.png, 50%"), """<img src="https://example.com/a.png" alt="https://example.com/a.png" onerror="this.onerror=null;this.src='/assets/img/attachmentFallback.svg';" style="width: 50%"/>""")
     val uptime = MacroUptime.toHtmlString("")
     assertEquals(MacroUptime.name, "Uptime")
     assertEquals(uptime.matches("""\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z"""), true)
--- app/logics/wikis/macros/MacroImage.scala
+++ app/logics/wikis/macros/MacroImage.scala
@@ -1,18 +1,29 @@
 package logics.wikis.macros
 
+import com.aha00a.commons.Implicits._
 import models.ContextWikiPage
 
 import scala.util.matching.Regex
 
 object MacroImage extends TraitMacro {
-  val regexWidth: Regex = """(.+),\s*(\d+(px|%)?)$""".r
+  private val fallbackImagePath: String = "/assets/img/attachmentFallback.svg"
+  private val regexWidth: Regex = """(.+),\s*(\d+(px|%)?)$""".r
+
+  private def imageTag(src: String, alt: String, widthOption: Option[String]): String = {
+    val href = src.escapeHtmlAttribute()
+    val escapedAlt = alt.escapeHtmlAttribute()
+    val fallbackImageSrc = fallbackImagePath.escapeHtmlAttribute()
+    val style = widthOption.map(width => s""" style="width: $width"""").getOrElse("")
+    s"""<img src="$href" alt="$escapedAlt" onerror="this.onerror=null;this.src='$fallbackImageSrc';"$style/>"""
+  }
 
   override def toHtmlString(argument: String)(implicit wikiContext: ContextWikiPage): String = {
-    //noinspection ScalaUnusedSymbol
-    argument match {
-      case regexWidth(url, width, null) => s"""<img src="$url" alt="$url" style="width: ${width}px"/>"""
-      case regexWidth(url, width, unit) => s"""<img src="$url" alt="$url" style="width: $width"/>"""
-      case _ => s"""<img src="$argument" alt="$argument"/>"""
+    val (url, widthOption) = argument match {
+      case regexWidth(rawUrl, width, null) => (rawUrl.trim, Some(s"${width}px"))
+      case regexWidth(rawUrl, width, unit) => (rawUrl.trim, Some(s"$width$unit"))
+      case _ => (argument.trim, None)
     }
+    val src = if (url.isEmpty) fallbackImagePath else url
+    imageTag(src, src, widthOption)
   }
 }
