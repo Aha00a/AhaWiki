212f4d1238b88f5ce23fe6edb0d5428564fa3f2f
aha00a <aha00a@gmail.com>
2026-05-20 22:31:04 +0900

    bump up java17

--- stage.sh
+++ stage.sh
@@ -3,5 +3,5 @@
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH="$JAVA_HOME/bin:$PATH"
 
-nice -n 19 ~/sbt/bin/sbt stage
+nice -n 19 "$JAVA_HOME/bin/java" -jar ~/sbt/bin/sbt-launch.jar stage
 
