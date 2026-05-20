b3b6615c220f047a4e865f2efe1d7ca142db4424
aha00a <aha00a@gmail.com>
2026-05-20 22:25:47 +0900

    bump up java17

--- start10000.sh
+++ start10000.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-target/universal/stage/bin/ahawiki -Dconfig.file=conf/wiki.aha00a.com.conf -Duser.timezone=Asia/Seoul -Dhttp.port=10000 -Dpidfile.path=RUNNING_PID_10000
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH="$JAVA_HOME/bin:$PATH"
+
+exec target/universal/stage/bin/ahawiki -Dconfig.file=conf/wiki.aha00a.com.conf -Duser.timezone=Asia/Seoul -Dhttp.port=10000 -Dpidfile.path=RUNNING_PID_10000
 
