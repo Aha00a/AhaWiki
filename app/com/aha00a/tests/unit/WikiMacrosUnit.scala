d59758aa6e7273f71ae2150473b3ceba19a8311e
aha00a <aha00a@gmail.com>
2026-05-25 21:16:44 +0900

    fix

--- app/com/aha00a/tests/unit/WikiMacrosUnit.scala
+++ app/com/aha00a/tests/unit/WikiMacrosUnit.scala
@@ -17,7 +17,7 @@ object WikiMacrosUnit {
     assertEquals(MacroCalendar.name, "Calendar")
     assertEquals(MacroWeekdayName.name, "WeekdayName")
     assertEquals(MacroColorCode.toHtmlString(empty), "")
-    assertEquals(MacroColorCode.toHtmlString("#fff"), """<span class="MacroColorCode"><spen class="preview" style="background-color: #fff"></spen> #fff</span>""")
+    assertEquals(MacroColorCode.toHtmlString("#fff"), """<span class="MacroColorCode"><span class="MacroColorCodeSwatch"><span style="background: #fff"></span></span><span class="MacroCopyable"><input class="auto resizeInputToContent" value="#fff" readonly="readonly"/><button type="button" aria-label="Copy" onclick="window.AhaWiki.Clipboard.copy(this.previousElementSibling.value);$(this).next().fadeIn();setTimeout(() =&gt; $(this).next().fadeOut(), 1000);"><i class="far fa-copy" aria-hidden="true"></i></button><span class="tooltip" onclick="$(this).fadeOut();">Copied!!</span></span></span>""")
     assertEquals(MacroColorCode.toHtmlString("#zzz"), """<div class="error">Argument Error - [[ColorCode(#zzz)]]</div>""")
     assertEquals(MacroKbd.name, "Kbd")
     assertEquals(MacroKbd.toHtmlString("Alt W"), """<kbd class="MacroKbd"><kbd>Alt</kbd><kbd>W</kbd></kbd>""")
--- app/logics/wikis/macros/MacroCopyable.scala
+++ app/logics/wikis/macros/MacroCopyable.scala
@@ -8,12 +8,7 @@ object MacroCopyable extends TraitMacro {
   }
 
   def doToHtmlString(argument: String): String = {
-    <span class="MacroCopyable">
-      <input class="auto resizeInputToContent" value={argument} readonly="readonly"/>
-      <button type="button" aria-label="Copy" onclick={"window.AhaWiki.Clipboard.copy(this.previousElementSibling.value);$(this).next().fadeIn();setTimeout(() => $(this).next().fadeOut(), 1000);"}>
-        <i class="far fa-copy" aria-hidden="true"></i>
-      </button>
-      <span class="tooltip" onclick="$(this).fadeOut();">Copied!!</span>
-    </span>.toString()
+    val escapedArgument = argument.replace("&", "&amp;").replace("\"", "&quot;")
+    s"""<span class="MacroCopyable"><input class="auto resizeInputToContent" value="$escapedArgument" readonly="readonly"/><button type="button" aria-label="Copy" onclick="window.AhaWiki.Clipboard.copy(this.previousElementSibling.value);$$(this).next().fadeIn();setTimeout(() =&gt; $$(this).next().fadeOut(), 1000);"><i class="far fa-copy" aria-hidden="true"></i></button><span class="tooltip" onclick="$$(this).fadeOut();">Copied!!</span></span>"""
   }
 }
