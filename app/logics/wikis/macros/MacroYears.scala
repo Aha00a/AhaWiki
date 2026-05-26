87e8546690e643f686dbd750a1e10252aaca42fe
aha00a <aha00a@gmail.com>
2026-05-27 03:11:10 +0900

    fix nofollow

--- app/logics/wikis/macros/MacroYears.scala
+++ app/logics/wikis/macros/MacroYears.scala
@@ -4,10 +4,12 @@ import logics.DefaultPageLogic
 import logics.wikis.interpreters.ahaMark.AhaMarkLink
 import models.ContextWikiPage
 import models.tables.CalculatedLink
+import models.tables.Site
 
 object MacroYears extends TraitMacro {
   override def isBlock: Boolean = true
   override def toHtmlString(argument:String)(implicit wikiContext: ContextWikiPage): String = { wikiContext.database.withConnection { implicit connection =>
+    implicit val site: Site = wikiContext.site
     CalculatedLink.selectDistinctDstWhereDstIsYear()
       .filter(v => wikiContext.setPageNameByPermission.contains(v) || DefaultPageLogic.isDefined(v))
       .map(name => AhaMarkLink(name, noFollow = true).toHtmlString())
