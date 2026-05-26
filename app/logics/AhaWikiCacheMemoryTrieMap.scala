ea5ab93bdaa18cf364a029dd4e167f63288387da
aha00a <aha00a@gmail.com>
2026-05-26 23:42:59 +0900

    fix cache problem

--- app/logics/AhaWikiCacheMemoryTrieMap.scala
+++ app/logics/AhaWikiCacheMemoryTrieMap.scala
@@ -1,13 +1,15 @@
 package logics
 
-import scala.collection.concurrent.TrieMap
+import java.util.concurrent.ConcurrentHashMap
+import scala.jdk.CollectionConverters._
 
 class AhaWikiCacheMemoryTrieMap[K, V] {
-  private val cache = TrieMap.empty[K, V]
+  private val cache = new ConcurrentHashMap[K, V]()
 
-  def getOrElseUpdate(key: K)(orElse: => V): V = cache.getOrElseUpdate(key, orElse)
+  def getOrElseUpdate(key: K)(orElse: => V): V =
+    cache.computeIfAbsent(key, _ => orElse)
   def invalidate(key: K): Unit = cache.remove(key)
   def clear(): Unit = cache.clear()
   def size: Int = cache.size
-  def values: Iterable[V] = cache.values
+  def values: Iterable[V] = cache.values().asScala
 }
