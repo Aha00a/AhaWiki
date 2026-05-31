177dbeb87cb585d44473b9559def4f7470cad218
aha00a <aha00a@gmail.com>
2026-05-31 18:54:38 +0900

    InterpreterPaper position variable

--- app/logics/wikis/interpreters/InterpreterPaper.scala
+++ app/logics/wikis/interpreters/InterpreterPaper.scala
@@ -24,25 +24,40 @@ object InterpreterPaper extends TraitInterpreter {
     val docId = pageContent.argument.lift(1).filter(_.nonEmpty)
       .getOrElse(eciv.variables.getOrElse("docId", ""))
 
+    // header/footer 6-position templates — {{pageNo}} 등이 페이지별로 치환됨
+    // topRight 기본값 = docId (하위호환), bottomRight 기본값 = {{pageNo}} (페이지번호 유지)
+    val topLeftRaw      = eciv.variables.getOrElse("topLeft",      "")
+    val topCenterRaw    = eciv.variables.getOrElse("topCenter",    "")
+    val topRightRaw     = eciv.variables.getOrElse("topRight",     docId)
+    val bottomLeftRaw   = eciv.variables.getOrElse("bottomLeft",   "")
+    val bottomCenterRaw = eciv.variables.getOrElse("bottomCenter", "")
+    val bottomRightRaw  = eciv.variables.getOrElse("bottomRight",  "{{pageNo}} / {{pageTotal}}")
+
     // Step 3: ---- 기준으로 split하되, 각 청크 앞에 \n × offset을 붙여 원본 줄 번호 보존
     //   lineOffset[N+1] = lineOffset[N] + chunks[N].count('\n')
     //   (chunk의 선행 \n이 separator 줄을 포함하므로 +1 불필요)
     val chunks = bodyExtracted.split("""(?m)^-{4,}$""", -1)
+    eciv.variables("pageTotal") = chunks.length.toString
     // lineOffset starts at the number of directive lines (#! lines stripped by PageContent),
     // so that padded content line numbers map to raw-document line numbers.
     var lineOffset = pageContent.directives.length
     val pages = chunks.zipWithIndex.map { case (chunk, index) =>
       val offset = lineOffset
       lineOffset += chunk.count(_ == '\n')
+      eciv.variables("pageNo") = (index + 1).toString
       val paddedResolved = "\n" * offset + eciv.applyVariables(chunk)
       val rendered = InterpreterWiki.toHtmlString(paddedResolved)
       s"""<div class="page">
          |  <!-- $index -->
          |  <div class="pageHeader">
-         |    <div class="documentId">$docId</div>
+         |    <div class="topLeft">${eciv.applyVariables(topLeftRaw)}</div>
+         |    <div class="topCenter">${eciv.applyVariables(topCenterRaw)}</div>
+         |    <div class="topRight">${eciv.applyVariables(topRightRaw)}</div>
          |  </div>
          |  <div class="pageFooter">
-         |    <div class="pageNo">${index + 1}</div>
+         |    <div class="bottomLeft">${eciv.applyVariables(bottomLeftRaw)}</div>
+         |    <div class="bottomCenter">${eciv.applyVariables(bottomCenterRaw)}</div>
+         |    <div class="bottomRight">${eciv.applyVariables(bottomRightRaw)}</div>
          |  </div>
          |  <div class="pageContent">
          |    <div>
