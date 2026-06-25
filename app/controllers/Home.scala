d40d111a30640b18d9e31a21bb6dbaa03a8223ee
aha00a <aha00a@gmail.com>
2026-06-26 08:11:51 +0900

    add sitemap.xml and update robots.txt handling

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -7,12 +7,10 @@ import logics.AhaWikiCache
 import logics.ApplicationConf
 import logics.SiteLogic
 import models.ContextSite
-import models.PageContent
 import models.WikiActors
 import models.tables.Site
 import play.api.Environment
 
-import java.io.File
 import play.api.db.Database
 import play.api.libs.ws.WSClient
 import play.api.mvc._
@@ -32,6 +30,14 @@ class Home @Inject() (
                        wsClient: WSClient,
                        executionContext: ExecutionContext
                       ) extends BaseController {
+  private def xmlEscape(value: String): String =
+    value
+      .replace("&", "&amp;")
+      .replace("<", "&lt;")
+      .replace(">", "&gt;")
+      .replace("\"", "&quot;")
+      .replace("'", "&apos;")
+
   def index: Action[AnyContent] = Action { implicit request =>
     Redirect(routes.Wiki.view("FrontPage", 0, "")).flashing(request.flash)
   }
@@ -44,13 +50,54 @@ class Home @Inject() (
   }
 
   def robotsTxt: Action[AnyContent] = Action { implicit request =>
-    database.withConnection { implicit connection =>
-      implicit val site: Site = SiteLogic.get(request.host)
+    val robots =
+      s"""User-agent: Linguee
+         |Disallow: /
+         |
+         |User-agent: SemrushBot
+         |Disallow: /
+         |
+         |User-agent: SemrushBot-SA
+         |Disallow: /
+         |
+         |User-agent: *
+         |Allow: /
+         |Disallow: /Admin
+         |Disallow: /api/
+         |Disallow: /actions/
+         |Disallow: /account
+         |Disallow: /login
+         |Disallow: /logout
+         |Disallow: /ws/
+         |Disallow: /dev/
+         |Disallow: /preview
+         |
+         |Sitemap: https://${request.host}/sitemap.xml
+         |""".stripMargin
 
-      Ok(models.tables.Page.selectLastRevision(".robots.txt").map(p => PageContent(p.content).content).getOrElse(
-        PageContent(new File("app/assets/Page", ".robots.txt").readAllString()).content
-      ))
-    }
+    Ok(robots).as("text/plain; charset=utf-8")
+  }
+
+  def sitemapXml: Action[AnyContent] = Action { implicit request =>
+    implicit val site: Site = SiteLogic.get(request.host)
+    val contextSite: ContextSite = ContextSite()
+    val host = xmlEscape(request.host)
+    val urls = contextSite.seqPageByPermission.map { page =>
+      val loc = s"https://$host/w/${UriUtil.encodeURIComponent(page.name)}"
+      val lastmod = page.dateTime.toLocalDate.toString
+      s"""  <url>
+         |    <loc>$loc</loc>
+         |    <lastmod>$lastmod</lastmod>
+         |  </url>""".stripMargin
+    }.mkString("\n")
+    val xml =
+      s"""<?xml version="1.0" encoding="UTF-8"?>
+         |<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+         |$urls
+         |</urlset>
+         |""".stripMargin
+
+    Ok(xml).as("application/xml; charset=utf-8")
   }
 
   def adsTxt: Action[AnyContent] = Action { implicit request =>
